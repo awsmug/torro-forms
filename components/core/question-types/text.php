c7a3f7135b07134a31e83a32493790c4ec775506
Sven Wagener <sven.wagener@rheinschmiede.de>
2014-02-03 19:57:47 +0100

    Added One Choice Question Type.

--- components/core/question-types/text.php
+++ components/core/question-types/text.php
@@ -40,3 +40,27 @@ class SurveyVal_QuestionType_MultipleChoice extends SurveyVal_QuestionType{
 	
 }
 sv_register_question_type( 'SurveyVal_QuestionType_MultipleChoice' );
+
+class SurveyVal_QuestionType_OneChoice extends SurveyVal_QuestionType{
+	
+	public function __construct( $id = null ){
+		$this->slug = 'OneChoice';
+		$this->title = __( 'One Choice', 'surveyval-locale' );
+		$this->description = __( 'Add a question which can be answered by selecting one of the given answers.', 'surveyval-locale' );
+		
+		$this->multiple_answers = TRUE;
+		$this->has_answers = TRUE;
+		
+		$this->answer_syntax = '<p><input type="radio" name="%s" value="%s" /> %s</p>';
+		$this->answer_params = array( 'name', 'value', 'answer' );
+		
+		$this->create_answer_syntax = '<p><input type="text" name="%s" value="%s" /></p>';
+		$this->create_answer_params = array( 'name', 'answer' );
+		
+		parent::__construct( $id );
+	}
+	
+}
+sv_register_question_type( 'SurveyVal_QuestionType_OneChoice' );
+
+
