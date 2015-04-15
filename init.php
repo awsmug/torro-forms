0d8fa6bb38aadfa4ba6cd72504f531f9318669ad
Sven Wagener <sven.wagener@rheinschmiede.de>
2015-04-15 19:59:00 +0200

    Changed to more secure getting path function

--- init.php
+++ init.php
@@ -263,9 +263,7 @@ class Questions_Init{
 	* @since 1.0.0
 	*/
 	private static function get_folder(){
-		$sub_folder = substr( dirname(__FILE__), strlen( ABSPATH ), ( strlen( dirname(__FILE__) ) - strlen( ABSPATH ) ) );
-		$script_folder = ABSPATH . $sub_folder;
-		return $script_folder;
+		return plugin_dir_path( __FILE__ );
 	}
 	
 	/**
