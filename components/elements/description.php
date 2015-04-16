7227b82be23a1362f8f1ff4c022636aa93161b07
bueltge <frank@bueltge.de>
2015-04-16 14:29:51 +0200

    format codex style

--- components/elements/description.php
+++ components/elements/description.php
@@ -1,48 +1,54 @@
 <?php
 /**
-* @package WordPress
-* @subpackage Questions
-* @author Sven Wagener
-* @copyright 2015, awesome.ug
-* @link http://awesome.ug
-* @license http://www.opensource.org/licenses/gpl-2.0.php GPL License
-*/
+ * @package    WordPress
+ * @subpackage Questions
+ * @author     Sven Wagener
+ * @copyright  2015, awesome.ug
+ * @link       http://awesome.ug
+ * @license    http://www.opensource.org/licenses/gpl-2.0.php GPL License
+ */
 
 // No direct access is allowed
-if( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-class Questions_SurveyElement_Description extends Questions_SurveyElement{
-	
-	public function __construct( $id = null ){
-		$this->slug = 'Description';
-		$this->title = __( 'Description', 'questions-locale' );
+class Questions_SurveyElement_Description extends Questions_SurveyElement {
+
+	public function __construct( $id = NULL ) {
+
+		$this->slug        = 'Description';
+		$this->title       = __( 'Description', 'questions-locale' );
 		$this->description = __( 'Adds a text to the form.', 'questions-locale' );
-		$this->icon = QUESTIONS_URLPATH . '/assets/images/icon-text.png';
-		
+		$this->icon        = QUESTIONS_URLPATH . '/assets/images/icon-text.png';
+
 		$this->is_question = FALSE;
-		
+
 		parent::__construct( $id );
 	}
-	
-	public function input_html(){
+
+	public function input_html() {
+
 		$html = '<div class="survey-element survey-element-' . $this->id . '">';
-		$html.= '<div class="survey-description">' . $this->settings['description'] . '</div>';
-		$html.= '</div>';
-		
+		$html .= '<div class="survey-description">' . $this->settings[ 'description' ] . '</div>';
+		$html .= '</div>';
+
 		return $html;
 	}
-	
-	public function settings_fields(){
+
+	public function settings_fields() {
+
 		$this->settings_fields = array(
 			'description' => array(
-				'title'			=> __( 'Text to show', 'questions-locale' ),
-				'type'			=> 'textarea',
-				'description' 	=> __( 'The text which will be shown in the form.', 'questions-locale' ),
-				'default'		=> ''
+				'title'       => __( 'Text to show', 'questions-locale' ),
+				'type'        => 'textarea',
+				'description' => __( 'The text which will be shown in the form.', 'questions-locale' ),
+				'default'     => ''
 			)
 		);
 	}
 }
+
 qu_register_survey_element( 'Questions_SurveyElement_Description' );
 
 
