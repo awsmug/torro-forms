30daaf44e5cdcaba1ddf9640c00f58090d5004f6
bueltge <frank@bueltge.de>
2015-04-16 14:26:24 +0200

    format codex style

--- components/elements/separator.php
+++ components/elements/separator.php
@@ -1,51 +1,58 @@
 <?php
 /**
-* @package WordPress
-* @subpackage Questions
-* @author Sven Wagener
-* @copyright 2015, awesome.ug
-* @link http://awesome.ug
-* @license http://www.opensource.org/licenses/gpl-2.0.php GPL License
-*/
+ * @package    WordPress
+ * @subpackage Questions
+ * @author     Sven Wagener
+ * @copyright  2015, awesome.ug
+ * @link       http://awesome.ug
+ * @license    http://www.opensource.org/licenses/gpl-2.0.php GPL License
+ */
 
 // No direct access is allowed
-if( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-class Questions_SurveyElement_Separator extends Questions_SurveyElement{
-	
-	public function __construct( $id = null ){
-		$this->slug = 'Separator';
-		$this->title = __( 'Separator', 'questions-locale' );
+class Questions_SurveyElement_Separator extends Questions_SurveyElement {
+
+	public function __construct( $id = NULL ) {
+
+		$this->slug        = 'Separator';
+		$this->title       = __( 'Separator', 'questions-locale' );
 		$this->description = __( 'Adds a optical separator (<hr>) between questions.', 'questions-locale' );
-		$this->icon = QUESTIONS_URLPATH . '/assets/images/icon-separator.png';
-		
+		$this->icon        = QUESTIONS_URLPATH . '/assets/images/icon-separator.png';
+
 		$this->is_question = FALSE;
-		
+
 		parent::__construct( $id );
 	}
-	
-	public function input_html(){
+
+	public function input_html() {
+
 		$html = '<div class="survey-element survey-element-' . $this->id . '">';
-		
-		if( !empty( $this->settings['header'] ) )
-			$html.= '<h3>' . $this->settings['header'] . '</h3>';
-			
-		$html.= '<hr /></div>';
-		
+
+		if ( ! empty( $this->settings[ 'header' ] ) ) {
+			$html .= '<h3>' . $this->settings[ 'header' ] . '</h3>';
+		}
+
+		$html .= '<hr /></div>';
+
 		return $html;
 	}
-	
-	public function settings_fields(){
+
+	public function settings_fields() {
+
 		$this->settings_fields = array(
 			'header' => array(
-				'title'			=> __( 'Headline', 'questions-locale' ),
-				'type'			=> 'text',
-				'description' 	=> __( 'Text which will be shown above the separator', 'questions-locale' ),
-				'default'		=> ''
+				'title'       => __( 'Headline', 'questions-locale' ),
+				'type'        => 'text',
+				'description' => __( 'Text which will be shown above the separator', 'questions-locale' ),
+				'default'     => ''
 			)
 		);
 	}
 }
+
 qu_register_survey_element( 'Questions_SurveyElement_Separator' );
 
 
