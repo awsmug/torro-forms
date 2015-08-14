84af1cb2a29317a6e454575d2f1e542fb9123524
Sven Wagener <sven.wagener@rheinschmiede.de>
2015-08-14 19:06:40 +0200

    Inbetween adding restrictions API

--- components/restrictions/base-restrictions/selected-members.php
+++ components/restrictions/base-restrictions/selected-members.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Restrict form to all members of site and does some checks
+ * Restrict form to all selected members
  *
  * Motherclass for all Restrictions
  *
@@ -39,25 +39,174 @@ class Questions_Restriction_SelectedMembers extends Questions_Restriction{
         $this->slug = 'selectedmembers';
 
         $this->option_name = __( 'Selected Members of site', 'wcsc-locale' );
+
+        add_action( 'admin_enqueue_scripts', array( $this , 'enqueue_scripts' ), 15 );
     }
 
     /**
      * Adds content to the option
      */
     public function option_content(){
-        return FALSE;
+        global $wpdb, $post, $questions_global;
+
+        $form_id = $post->ID;
+
+        $sql      = $wpdb->prepare(  "SELECT user_id FROM {$questions_global->tables->participiants} WHERE survey_id = %s", $form_id );
+        $user_ids = $wpdb->get_col( $sql );
+
+        $users = array();
+
+        if ( is_array( $user_ids ) && count( $user_ids ) > 0 ) {
+            $users = get_users(
+                array(
+                    'include' => $user_ids,
+                    'orderby' => 'ID'
+                )
+            );
+        }
+
+        $options = apply_filters( 'questions_add_participiants_options',
+                        array(
+                            'all_members' => esc_attr__( 'Add all actual Members', 'questions-locale' )
+                        )
+        );
+
+        $selected = '';
+
+        // Participiants select method
+        $html = '<div id="questions_participiants_select">';
+            $html .= '<select name="questions_participiants_select" id="questions-participiants-select">';
+            foreach ( $options AS $key => $value ):
+                $html .= '<option value="' . $key . '"' . $selected . '>' . $value . '</option>';
+            endforeach;
+            $html .= '</select>';
+        $html .= '</div>';
+
+        // Action Buttons
+        $html .= '<div id="questions-participiants-standard-options" class="questions-participiants-options-content">';
+        $html .= '<div class="add">
+                        <input type="button" class="questions-add-participiants button" id="questions-add-members-standard" value="' . esc_attr__('Add Participiants', 'questions-locale') . '" />
+                        <a href="#" class="questions-remove-all-participiants">' . esc_attr__('Remove all Participiants', 'questions-locale') . '</a></div>
+                  </div>';
+
+        // Hooking in
+        ob_start();
+        do_action( 'questions_participiants_content_top' );
+        $html .= ob_get_clean();
+
+        // User statistic
+        $html .= '<div id="questions-participiants-status" class="questions-participiants-status">';
+        $html .= '<p>' . count( $users ) . ' ' . esc_attr__( 'participiant/s', 'questions-locale' ) . '</p>';
+        $html .= '</div>';
+
+        // Participiants list
+        $html .= '<div id="questions-participiants-list">';
+        $html .= '<table class="wp-list-table widefat">';
+        $html .= '<thead>';
+        $html .= '<tr>';
+        $html .= '<th>' . esc_attr__( 'ID', 'questions-locale' ) . '</th>';
+        $html .= '<th>' . esc_attr__( 'User nicename', 'questions-locale' ) . '</th>';
+        $html .= '<th>' . esc_attr__( 'Display name', 'questions-locale' ) . '</th>';
+        $html .= '<th>' . esc_attr__( 'Email', 'questions-locale' ) . '</th>';
+        $html .= '<th>' . esc_attr__( 'Status', 'questions-locale' ) . '</th>';
+        $html .= '<th>&nbsp</th>';
+        $html .= '</tr>';
+        $html .= '</thead>';
+
+        $html .= '<tbody>';
+
+        $questions_participiants_value = '';
+
+        if ( is_array( $users ) && count( $users ) > 0 ):
+
+            foreach ( $users AS $user ):
+                if ( qu_user_has_participated( $form_id, $user->ID ) ):
+                    $user_css  = ' finished';
+                    $user_text = esc_attr__( 'finished', 'questions-locale' );
+                else:
+                    $user_text = esc_attr__( 'new', 'questions-locale' );
+                    $user_css  = ' new';
+                endif;
+
+                $html .= '<tr class="participiant participiant-user-' . $user->ID . $user_css . '">';
+                $html .= '<td>' . $user->ID . '</td>';
+                $html .= '<td>' . $user->user_nicename . '</td>';
+                $html .= '<td>' . $user->display_name . '</td>';
+                $html .= '<td>' . $user->user_email . '</td>';
+                $html .= '<td>' . $user_text . '</td>';
+                $html .= '<td><a class="button questions-delete-participiant" rel="' . $user->ID . '">' . esc_attr__(
+                        'Delete', 'questions-locale'
+                    ) . '</a></th>';
+                $html .= '</tr>';
+            endforeach;
+
+            $questions_participiants_value = implode( ',', $user_ids );
+
+        endif;
+
+        $html .= '</tbody>';
+
+        $html .= '</table>';
+
+        $html .= '<input type="hidden" id="questions-participiants" name="questions_participiants" value="' . $questions_participiants_value . '" />';
+        $html .= '<input type="hidden" id="questions-participiants-count" name="questions-participiants-count" value="' . count($users) . '" />';
+
+        $html .= '</div>';
+
+        return $html;
     }
 
     /**
      * Checks if the user can pass
      */
     public function check(){
+
         if ( ! is_user_logged_in() ){
-            $this->add_message( 'error', esc_attr( 'You have to be logged in to participate.', 'wcsc-locale' ) );
+            $this->add_message( 'error', esc_attr( 'You have to be logged in to participate.', 'questions-locale' ) );
             return FALSE;
         }
+
+        if ( ! is_participiant() ) {
+            $this->add_message('error', esc_attr('You can\'t participate this survey.', 'questions-locale'));
+        }
+
         return TRUE;
     }
 
+    /**
+     * Checks if a user can participate
+     *
+     * @param int $form_id
+     * @param int $user_id
+     * @return boolean $can_participate
+     * @since 1.0.0
+     */
+    public function is_participiant( $user_id = NULL ) {
+
+        global $wpdb, $current_user, $questions_global, $questions_form_id;
+
+        $is_participiant = FALSE;
+
+        // Setting up user ID
+        if ( NULL == $user_id ):
+            get_currentuserinfo();
+            $user_id = $user_id = $current_user->ID;
+        endif;
+
+        $sql = $wpdb->prepare( "SELECT user_id FROM {$questions_global->tables->participiants} WHERE survey_id = %d", $is_participiant );
+        $user_ids = $wpdb->get_col( $sql );
+
+        if( in_array( $user_id, $user_ids  ) )
+            $is_participiant = TRUE;
+
+        return apply_filters( 'questions_user_is_participiant', $is_participiant, $user_id );
+    }
+
+    /**
+     * Enqueue Scripts
+     */
+    public function enqueue_scripts(){
+        wp_enqueue_script( 'questions-selected-members', QUESTIONS_URLPATH . '/components/restrictions/base-restrictions/includes/js/selected-members.js' );
+    }
 }
 qu_register_restriction( 'Questions_Restriction_SelectedMembers' );
--- components/restrictions/form-builder-extension.php
+++ components/restrictions/form-builder-extension.php
@@ -43,7 +43,6 @@ class Questions_FormBuilder_RestrictionsExtension{
         if ( ! is_admin() )
             return NULL;
 
-        add_action( 'admin_enqueue_scripts', array( __CLASS__ , 'enqueue_scripts' ), 15 );
         add_action( 'add_meta_boxes', array( __CLASS__, 'meta_boxes' ), 15 );
 
         add_action( 'save_post', array( __CLASS__, 'save_form' ) );
@@ -95,6 +94,10 @@ class Questions_FormBuilder_RestrictionsExtension{
          */
         $selected_restriction = get_post_meta( $form_id, 'participiant_restrictions', TRUE );
 
+        if ( '' == $selected_restriction ): // If there was selected nothing before
+            $selected_restriction = 'allvisitors';
+        endif;
+
         $html  = '<div id="questions_restrictions">';
         $html .= '<select name="questions_restrictions_select" id="questions-restrictions-select">';
         foreach( $restrictions AS $slug => $restriction ) {
@@ -121,169 +124,6 @@ class Questions_FormBuilder_RestrictionsExtension{
             $html .= '<div id="questions_restrictions_content_' . $restriction->slug . '" class="questions-restrictions-content questions-restrictions-content-' . $restriction->slug . '">' . $option_content . '</div>';
         }
 
-
-        /*
-
-        $form_id = $post->ID;
-
-        $sql      = $wpdb->prepare(  "SELECT user_id FROM {$questions_global->tables->participiants} WHERE survey_id = %s", $form_id );
-        $user_ids = $wpdb->get_col( $sql );
-
-        $users = array();
-
-        if ( is_array( $user_ids ) && count( $user_ids ) > 0 ):
-            $users = get_users(
-                array(
-                    'include' => $user_ids,
-                    'orderby' => 'ID'
-                )
-            );
-        endif;
-
-        $disabled = '';
-        $selected = '';
-
-        $participiant_restrictions = get_post_meta( $form_id, 'participiant_restrictions', TRUE );
-
-        $restrictions = apply_filters(
-            'questions_post_type_participiant_restrictions',
-            array(
-                'all_visitors'     => esc_attr__(
-                    'All visitors of the site can participate',
-                    'questions-locale'
-                ),
-                'all_members'      => esc_attr__(
-                    'All members of the site can participate',
-                    'questions-locale'
-                ),
-                'selected_members' => esc_attr__(
-                    'Only selected members can participate',
-                    'questions-locale'
-                ),
-                'no_restrictions'     => esc_attr__(
-                    'No restrictions',
-                    'questions-locale'
-                )
-            )
-        );
-
-        if ( '' == $participiant_restrictions && count( $users ) > 0 ): // If there are participiants and nothing was selected before
-            $participiant_restrictions = 'selected_members';
-        elseif ( '' == $participiant_restrictions ): // If there was selected nothing before
-            $participiant_restrictions = 'all_visitors';
-        endif;
-
-        $html = '<div id="questions_participiants_select_restrictions">';
-        $html .= '<select name="questions_participiants_restrictions_select" id="questions-participiants-restrictions-select"' . $disabled . '>';
-        foreach ( $restrictions AS $key => $value ):
-            $selected = '';
-            if ( $key == $participiant_restrictions ) {
-                $selected = ' selected="selected"';
-            }
-            $html .= '<option value="' . $key . '"' . $selected . '>' . $value . '</option>';
-        endforeach;
-        $html .= '</select>';
-        $html .= '</div>';
-
-        $options = apply_filters(
-            'questions_post_type_add_participiants_options',
-            array(
-                'all_members' => esc_attr__(
-                    'Add all actual Members', 'questions-locale'
-                ),
-            )
-        );
-
-        $html .= '<div id="questions_selected_members">';
-
-        $disabled = '';
-        $selected = '';
-
-        $html .= '<div id="questions_participiants_select">';
-        $html .= '<select name="questions_participiants_select" id="questions-participiants-select"' . $disabled . '>';
-        foreach ( $options AS $key => $value ):
-            $html .= '<option value="' . $key . '"' . $selected . '>' . $value . '</option>';
-        endforeach;
-        $html .= '</select>';
-        $html .= '</div>';
-
-        $html .= '<div id="questions-participiants-standard-options" class="questions-participiants-options-content">';
-        $html .= '<div class="add"><input type="button" class="questions-add-participiants button" id="questions-add-members-standard" value="'
-            . esc_attr__(
-                'Add Participiants', 'questions-locale'
-            ) . '" /><a href="#" class="questions-remove-all-participiants">'
-            . esc_attr__(
-                'Remove all Participiants', 'questions-locale'
-            ) . '</a></div>';
-        $html .= '</div>';
-
-        ob_start();
-        do_action( 'questions_post_type_participiants_content_top' );
-        $html .= ob_get_clean();
-
-        $html .= '<div id="questions-participiants-status" class="questions-participiants-status">';
-        $html .= '<p>' . count( $users ) . ' ' . esc_attr__( 'participiant/s', 'questions-locale' ) . '</p>';
-        $html .= '</div>';
-
-        $html .= '<div id="questions-participiants-list">';
-        $html .= '<table class="wp-list-table widefat">';
-        $html .= '<thead>';
-        $html .= '<tr>';
-        $html .= '<th>' . esc_attr__( 'ID', 'questions-locale' ) . '</th>';
-        $html .= '<th>' . esc_attr__( 'User nicename', 'questions-locale' ) . '</th>';
-        $html .= '<th>' . esc_attr__( 'Display name', 'questions-locale' ) . '</th>';
-        $html .= '<th>' . esc_attr__( 'Email', 'questions-locale' ) . '</th>';
-        $html .= '<th>' . esc_attr__( 'Status', 'questions-locale' ) . '</th>';
-        $html .= '<th>&nbsp</th>';
-        $html .= '</tr>';
-        $html .= '</thead>';
-
-        $html .= '<tbody>';
-
-        $questions_participiants_value = '';
-
-        if ( is_array( $users ) && count( $users ) > 0 ):
-
-            foreach ( $users AS $user ):
-                if ( qu_user_has_participated( $form_id, $user->ID ) ):
-                    $user_css  = ' finished';
-                    $user_text = esc_attr__( 'finished', 'questions-locale' );
-                else:
-                    $user_text = esc_attr__( 'new', 'questions-locale' );
-                    $user_css  = ' new';
-                endif;
-
-                $html .= '<tr class="participiant participiant-user-' . $user->ID . $user_css . '">';
-                $html .= '<td>' . $user->ID . '</td>';
-                $html .= '<td>' . $user->user_nicename . '</td>';
-                $html .= '<td>' . $user->display_name . '</td>';
-                $html .= '<td>' . $user->user_email . '</td>';
-                $html .= '<td>' . $user_text . '</td>';
-                $html .= '<td><a class="button questions-delete-participiant" rel="' . $user->ID . '">' . esc_attr__(
-                        'Delete', 'questions-locale'
-                    ) . '</a></th>';
-                $html .= '</tr>';
-            endforeach;
-
-            $questions_participiants_value = implode( ',', $user_ids );
-
-        endif;
-
-        $html .= '</tbody>';
-
-        $html .= '</table>';
-
-        $html .= '<input type="hidden" id="questions-participiants" name="questions_participiants" value="' . $questions_participiants_value . '" />';
-        $html .= '<input type="hidden" id="questions-participiants-count" name="questions-participiants-count" value="' . count(
-                $users
-            ) . '" />';
-
-        $html .= '</div>';
-
-        $html .= '</div>';
-
-
-        */
         echo $html;
     }
 }
