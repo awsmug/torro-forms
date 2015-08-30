2e0af62cd45fc2fe04ee0f273ba968d6b7785a0a
Sven Wagener <sven.wagener@rheinschmiede.de>
2015-08-30 15:28:43 +0200

    Added PHPDoc comments to Elements

--- core/elements/base-elements/separator.php
+++ core/elements/base-elements/separator.php
@@ -1,11 +1,27 @@
 <?php
 /**
- * @package    WordPress
- * @subpackage Questions
- * @author     Sven Wagener
- * @copyright  2015, awesome.ug
- * @link       http://awesome.ug
- * @license    http://www.opensource.org/licenses/gpl-2.0.php GPL License
+ * Separator Form Element
+ *
+ * @author  awesome.ug, Author <support@awesome.ug>
+ * @package AwesomeForms/Core/Elements
+ * @version 1.0.0
+ * @since   1.0.0
+ * @license GPL 2
+ *
+ * Copyright 2015 awesome.ug (support@awesome.ug)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 // No direct access is allowed
