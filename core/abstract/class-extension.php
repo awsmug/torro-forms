6ea1acb7baeecca0a6ffd9683f7884eda7d3818f
Sven Wagener <sven.wagener@rheinschmiede.de>
2016-01-17 22:35:48 +0100

    Only starting EDD updater if a license key was added

--- core/abstract/class-extension.php
+++ core/abstract/class-extension.php
@@ -120,18 +120,19 @@ abstract class Torro_Extension extends Torro_Instance {
 
 		if( !empty( $license_key ) ){
 			$license_key = trim( $license_key );
-		}
 
-		$edd_updater = new EDD_SL_Plugin_Updater( 'http://torro-forms.com', $this->plugin_file, array(
-			'version'   => $this->version,
-			// current version number
-			'license'   => $license_key,
-			// license key (used get_option above to retrieve from DB)
-			'item_name' => $this->item_name,
-			// name of this plugin
-			'author'    => 'Awesome UG'
-			// author of this plugin
-		) );
+
+			$edd_updater = new EDD_SL_Plugin_Updater( 'http://torro-forms.com', $this->plugin_file, array(
+				'version'   => $this->version,
+				// current version number
+				'license'   => $license_key,
+				// license key (used get_option above to retrieve from DB)
+				'item_name' => $this->item_name,
+				// name of this plugin
+				'author'    => 'Awesome UG'
+				// author of this plugin
+			) );
+		}
 	}
 
 	/**
