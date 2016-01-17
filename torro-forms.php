880ac0b3ac7ebb631539a34c6fb037f8df55f5b8
Sven Wagener <sven.wagener@rheinschmiede.de>
2016-01-17 15:57:12 +0100

    Added missing comments

--- torro-forms.php
+++ torro-forms.php
@@ -18,8 +18,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Torro_Init {
+	/**
+	 * @var bool $tables_registered
+	 * @since 1.0.0
+	 */
 	private static $tables_registered = false;
 
+	/**
+	 * @var bool $post_types_registered
+	 * @since 1.0.0
+	 */
 	private static $post_types_registered = false;
 
 	/**
@@ -54,6 +62,11 @@ class Torro_Init {
 		return load_plugin_textdomain( 'torro-forms', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 	}
 
+	/**
+	 * Registering Tables to $wpdb
+	 *
+	 * @since 1.0.0
+	 */
 	private static function register_tables() {
 		global $wpdb;
 
@@ -68,11 +81,16 @@ class Torro_Init {
 		foreach ( $tables as $table ) {
 			$table_name = 'torro_' . $table;
 
-			$wpdb->tables[] = $table_name;
+			$wpdb->tables[]    = $table_name;
 			$wpdb->$table_name = $wpdb->prefix . $table_name;
 		}
 	}
 
+	/**
+	 * Getting all DB tables form plugin
+	 *
+	 * @return array
+	 */
 	private static function get_tables() {
 		$tables = $orig_tables = array(
 			'elements',
@@ -93,14 +111,16 @@ class Torro_Init {
 
 	/**
 	 * Including files of component
+	 *
+	 * @since 1.0.0
 	 */
 	private static function includes() {
 		require_once( plugin_dir_path( __FILE__ ) . 'core/error.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'core/ajax.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'core/torro.php' );
 
-		$includes_folder = torro()->path( 'includes/' );
-		$core_folder = torro()->path( 'core/' );
+		$includes_folder   = torro()->path( 'includes/' );
+		$core_folder       = torro()->path( 'core/' );
 		$components_folder = torro()->path( 'components/' );
 
 		// Functions
@@ -158,94 +178,12 @@ class Torro_Init {
 	}
 
 	/**
-	 * Creates Custom Post Types for Torro Forms
-	 *
-	 * @since 1.0.0
-	 */
-	public static function custom_post_types() {
-		if ( self::$post_types_registered ) {
-			return;
-		}
-
-		// torro_get_settings() might not be available here, so do it plain WordPress
-		$slug = get_option( 'torro_settings_general_slug', 'forms' );
-
-		/**
-		 * Post Types
-		 */
-		$args_post_type = array(
-			'labels'				=> array(
-				'name'					=> __( 'Forms', 'torro-forms' ),
-				'singular_name'			=> __( 'Form', 'torro-forms' ),
-				'add_new'				=> __( 'Add New', 'torro-forms' ),
-				'add_new_item'			=> __( 'Add New Form', 'torro-forms' ),
-				'edit_item'				=> __( 'Edit Form', 'torro-forms' ),
-				'new_item'				=> __( 'New Form', 'torro-forms' ),
-				'view_item'				=> __( 'View Form', 'torro-forms' ),
-				'search_items'			=> __( 'Search Forms', 'torro-forms' ),
-				'not_found'				=> __( 'No forms found.', 'torro-forms' ),
-				'not_found_in_trash'	=> __( 'No forms found in Trash.', 'torro-forms' ),
-				'all_items'				=> __( 'All Forms', 'torro-forms' ),
-				'archives'				=> __( 'Form Archives', 'torro-forms' ),
-				'insert_into_item'		=> __( 'Insert into form', 'torro-forms' ),
-				'uploaded_to_this_item'	=> __( 'Uploaded to this form', 'torro-forms' ),
-				'filter_items_list'		=> __( 'Filter forms list', 'torro-forms' ),
-				'items_list_navigation'	=> __( 'Forms list navigation', 'torro-forms' ),
-				'items_list'			=> __( 'Forms list', 'torro-forms' ),
-				'menu_name'				=> __( 'Forms', 'torro-forms' ),
-			),
-			'public'				=> true,
-			'has_archive'			=> true,
-			'supports'				=> array( 'title' ),
-			'show_in_menu'			=> true,
-			'show_in_nav_menus'		=> false,
-			'rewrite'				=> array( 'slug' => $slug, 'with_front' => true ),
-			'menu_position'			=> 50,
-		);
-
-		register_post_type( 'torro-forms', $args_post_type );
-
-		/**
-		 * Categories
-		 */
-		$args_taxonomy = array(
-			'show_in_nav_menus'		=> true,
-			'hierarchical'			=> true,
-			'labels'				=> array(
-				'name'					=> __( 'Categories', 'torro-forms' ),
-				'singular_name'			=> __( 'Category', 'torro-forms' ),
-				'search_items'			=> __( 'Search Categories', 'torro-forms' ),
-				'popular_items'			=> __( 'Popular Categories', 'torro-forms' ),
-				'all_items'				=> __( 'All Categories', 'torro-forms' ),
-				'parent_item'			=> __( 'Parent Category', 'torro-forms' ),
-				'parent_item_colon'		=> __( 'Parent Category:', 'torro-forms' ),
-				'edit_item'				=> __( 'Edit Category', 'torro-forms' ),
-				'view_item'				=> __( 'View Category', 'torro-forms' ),
-				'update_item'			=> __( 'Update Category', 'torro-forms' ),
-				'add_new_item'			=> __( 'Add New Category', 'torro-forms' ),
-				'new_item_name'			=> __( 'New Category', 'torro-forms' ),
-				'not_found'				=> __( 'No categories found.', 'torro-forms' ),
-				'no_terms'				=> __( 'No categories', 'torro-forms' ),
-				'items_list_navigation'	=> __( 'Categories list navigation', 'torro-forms' ),
-				'items_list'			=> __( 'Categories list', 'torro-forms' ),
-				'menu_name'				=> __( 'Categories', 'torro-forms' ),
-			),
-			'show_ui'				=> true,
-			'query_var'				=> true,
-			'rewrite'				=> true,
-		);
-
-		register_taxonomy( 'torro-forms-categories', array( 'torro-forms' ), $args_taxonomy );
-
-		self::$post_types_registered = true;
-	}
-
-	/**
 	 * Adding CSS Classes to body
 	 *
 	 * @param array $classes Classes for body
 	 *
 	 * @return array $classes Classes for body
+	 * @since 1.0.0
 	 */
 	public static function add_body_class( $classes ) {
 		global $post;
@@ -282,7 +220,9 @@ class Torro_Init {
 	/**
 	 * Fired when the plugin is activated.
 	 *
-	 * @param boolean $network_wide True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
+	 * @param boolean $network_wide True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled
+	 *                              or plugin is activated on an individual blog
+	 *
 	 * @since 1.0.0
 	 */
 	public static function activate( $network_wide ) {
@@ -295,25 +235,45 @@ class Torro_Init {
 	}
 
 	/**
-	 * Fired when the plugin is deactivated.
+	 * Setting up base plugin data
 	 *
-	 * @param boolean $network_wide True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
 	 * @since 1.0.0
 	 */
-	public static function deactivate( $network_wide ) {
-		delete_option( 'torro_is_installed' );
+	private static function setup() {
+		$script_db_version  = '1.0.3';
+		$current_db_version = get_option( 'torro_db_version' );
 
-		add_action( 'shutdown', 'flush_rewrite_rules' );
+		// Upgrading from Questions to Awesome Forms
+		if ( false !== get_option( 'questions_db_version' ) ) {
+			require_once( 'includes/updates/to-awesome-forms.php' );
+			torro_questions_to_awesome_forms();
+
+			update_option( 'af_db_version', '1.0.1' );
+			delete_option( 'questions_db_version' );
+		}
+
+		// Upgrading form Awesome Forms to Torro Forms
+		if ( false !== get_option( 'af_db_version' ) ) {
+			require_once( 'includes/updates/to-torro-forms.php' );
+			awesome_forms_to_torro_forms();
+
+			update_option( 'torro_db_version', '1.0.2' );
+			delete_option( 'af_db_version' );
+		}
+
+		// Upgrading from Torro DB version 1.0.2 to 1.0.3
+		if ( true === version_compare( $current_db_version, '1.0.3', '<' ) ) {
+			require_once( 'includes/updates/to_1.0.3.php' );
+			awesome_forms_to_1_0_3();
+			update_option( 'torro_db_version', $script_db_version );
+		}
+
+		// Fresh Torro DB install
+		if ( false === $current_db_version || false === self::is_installed() ) {
+			self::install_tables();
+			update_option( 'torro_db_version', $script_db_version );
+		}
 	}
-
-	/**
-	 * Fired when the plugin is uninstalled.
-	 *
-	 * @param boolean $network_wide True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
-	 * @since 1.0.0
-	 */
-	public static function uninstall( $network_wide ) {}
-
 
 	/**
 	 * Checking if the plugin already installed
@@ -338,47 +298,8 @@ class Torro_Init {
 	}
 
 	/**
-	 * Setting up base plugin data
-	 * @since 1.0.0
-	 */
-	private static function setup() {
-		$script_db_version = '1.0.3';
-		$current_db_version  = get_option( 'torro_db_version' );
-
-		// Upgrading from Questions to Awesome Forms
-		if ( false !== get_option( 'questions_db_version' ) ) {
-			require_once( 'includes/updates/to-awesome-forms.php' );
-			torro_questions_to_awesome_forms();
-
-			update_option( 'af_db_version', '1.0.1' );
-			delete_option( 'questions_db_version' );
-		}
-
-		// Upgrading form Awesome Forms to Torro Forms
-		if ( false !== get_option( 'af_db_version' ) ) {
-			require_once( 'includes/updates/to-torro-forms.php' );
-			awesome_forms_to_torro_forms();
-
-			update_option( 'torro_db_version', '1.0.2' );
-			delete_option( 'af_db_version' );
-		}
-
-		// Upgrading from Torro DB version 1.0.2 to 1.0.3
-		if( true === version_compare( $current_db_version, '1.0.3', '<' ) ){
-			require_once( 'includes/updates/to_1.0.3.php' );
-			awesome_forms_to_1_0_3();
-			update_option( 'torro_db_version', $script_db_version );
-		}
-
-		// Fresh Torro DB install
-		if ( false === $current_db_version || false === self::is_installed() ) {
-			self::install_tables();
-			update_option( 'torro_db_version', $script_db_version );
-		}
-	}
-
-	/**
 	 * Installing tables
+	 * @since 1.0.0
 	 */
 	private static function install_tables() {
 		global $wpdb;
@@ -468,7 +389,9 @@ class Torro_Init {
 
 	/**
 	 * Getting charset from DB
+	 *
 	 * @return string
+	 * @since 1.0.0
 	 */
 	private static function get_charset_collate() {
 		global $wpdb;
@@ -485,6 +408,114 @@ class Torro_Init {
 
 		return $charset_collate;
 	}
+
+	/**
+	 * Creates Custom Post Types for Torro Forms
+	 *
+	 * @since 1.0.0
+	 */
+	public static function custom_post_types() {
+		if ( self::$post_types_registered ) {
+			return;
+		}
+
+		// torro_get_settings() might not be available here, so do it plain WordPress
+		$slug = get_option( 'torro_settings_general_slug', 'forms' );
+
+		/**
+		 * Post Types
+		 */
+		$args_post_type = array(
+			'labels'            => array(
+				'name'                  => __( 'Forms', 'torro-forms' ),
+				'singular_name'         => __( 'Form', 'torro-forms' ),
+				'add_new'               => __( 'Add New', 'torro-forms' ),
+				'add_new_item'          => __( 'Add New Form', 'torro-forms' ),
+				'edit_item'             => __( 'Edit Form', 'torro-forms' ),
+				'new_item'              => __( 'New Form', 'torro-forms' ),
+				'view_item'             => __( 'View Form', 'torro-forms' ),
+				'search_items'          => __( 'Search Forms', 'torro-forms' ),
+				'not_found'             => __( 'No forms found.', 'torro-forms' ),
+				'not_found_in_trash'    => __( 'No forms found in Trash.', 'torro-forms' ),
+				'all_items'             => __( 'All Forms', 'torro-forms' ),
+				'archives'              => __( 'Form Archives', 'torro-forms' ),
+				'insert_into_item'      => __( 'Insert into form', 'torro-forms' ),
+				'uploaded_to_this_item' => __( 'Uploaded to this form', 'torro-forms' ),
+				'filter_items_list'     => __( 'Filter forms list', 'torro-forms' ),
+				'items_list_navigation' => __( 'Forms list navigation', 'torro-forms' ),
+				'items_list'            => __( 'Forms list', 'torro-forms' ),
+				'menu_name'             => __( 'Forms', 'torro-forms' ),
+			),
+			'public'            => true,
+			'has_archive'       => true,
+			'supports'          => array( 'title' ),
+			'show_in_menu'      => true,
+			'show_in_nav_menus' => false,
+			'rewrite'           => array( 'slug' => $slug, 'with_front' => true ),
+			'menu_position'     => 50,
+		);
+
+		register_post_type( 'torro-forms', $args_post_type );
+
+		/**
+		 * Categories
+		 */
+		$args_taxonomy = array(
+			'show_in_nav_menus' => true,
+			'hierarchical'      => true,
+			'labels'            => array(
+				'name'                  => __( 'Categories', 'torro-forms' ),
+				'singular_name'         => __( 'Category', 'torro-forms' ),
+				'search_items'          => __( 'Search Categories', 'torro-forms' ),
+				'popular_items'         => __( 'Popular Categories', 'torro-forms' ),
+				'all_items'             => __( 'All Categories', 'torro-forms' ),
+				'parent_item'           => __( 'Parent Category', 'torro-forms' ),
+				'parent_item_colon'     => __( 'Parent Category:', 'torro-forms' ),
+				'edit_item'             => __( 'Edit Category', 'torro-forms' ),
+				'view_item'             => __( 'View Category', 'torro-forms' ),
+				'update_item'           => __( 'Update Category', 'torro-forms' ),
+				'add_new_item'          => __( 'Add New Category', 'torro-forms' ),
+				'new_item_name'         => __( 'New Category', 'torro-forms' ),
+				'not_found'             => __( 'No categories found.', 'torro-forms' ),
+				'no_terms'              => __( 'No categories', 'torro-forms' ),
+				'items_list_navigation' => __( 'Categories list navigation', 'torro-forms' ),
+				'items_list'            => __( 'Categories list', 'torro-forms' ),
+				'menu_name'             => __( 'Categories', 'torro-forms' ),
+			),
+			'show_ui'           => true,
+			'query_var'         => true,
+			'rewrite'           => true,
+		);
+
+		register_taxonomy( 'torro-forms-categories', array( 'torro-forms' ), $args_taxonomy );
+
+		self::$post_types_registered = true;
+	}
+
+	/**
+	 * Fired when the plugin is deactivated.
+	 *
+	 * @param boolean $network_wide True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled
+	 *                              or plugin is activated on an individual blog
+	 *
+	 * @since 1.0.0
+	 */
+	public static function deactivate( $network_wide ) {
+		delete_option( 'torro_is_installed' );
+
+		add_action( 'shutdown', 'flush_rewrite_rules' );
+	}
+
+	/**
+	 * Fired when the plugin is uninstalled.
+	 *
+	 * @param boolean $network_wide True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled
+	 *                              or plugin is activated on an individual blog
+	 *
+	 * @since 1.0.0
+	 */
+	public static function uninstall( $network_wide ) {
+	}
 }
 
 register_activation_hook( __FILE__, array( 'Torro_Init', 'activate' ) );
@@ -494,4 +525,5 @@ register_uninstall_hook( __FILE__, array( 'Torro_Init', 'uninstall' ) );
 function torro_init() {
 	Torro_Init::init();
 }
+
 add_action( 'plugins_loaded', 'torro_init' );
