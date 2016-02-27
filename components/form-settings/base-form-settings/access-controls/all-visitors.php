78c74e0a78641f96d0859d96d680fc54640cec25
Felix Arntz <felix-arntz@t-online.de>
2016-02-27 15:55:53 +0100

    fixed fingerprint2 asset URL

--- components/form-settings/base-form-settings/access-controls/all-visitors.php
+++ components/form-settings/base-form-settings/access-controls/all-visitors.php
@@ -105,7 +105,7 @@ final class Torro_Access_Control_All_Visitors extends Torro_Access_Control {
 	 * Loading fingerprint scripts
 	 */
 	public function frontend_scripts() {
-		wp_enqueue_script( 'fingerprintjs2', torro()->get_asset_url( 'fingerprintjs2/dist/fingerprintjs2.min', 'vendor-js', true ) );
+		wp_enqueue_script( 'fingerprintjs2', torro()->get_asset_url( 'fingerprintjs2/dist/fingerprint2.min', 'vendor-js', true ) );
 	}
 
 	/**
