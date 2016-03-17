42f638b4dea6dc0352149ecc1aff809481059f50
Sven Wagener <sven.wagener@rheinschmiede.de>
2016-03-17 13:34:31 +0100

    Formatted code

--- assets/src/js/templatetags.js
+++ assets/src/js/templatetags.js
@@ -1,13 +1,11 @@
-(function (exports, wp, $, translations) {
+(function( exports, wp, $, translations ){
 	'use strict';
 
 	/**
 	 * Form_Builder constructor
 	 */
-	function Torro_Templatetags(translations) {
+	function Torro_Templatetags( translations ){
 		this.translations = translations;
-
-		this.extensions = {};
 
 		this.selectors = {};
 	}
@@ -16,51 +14,51 @@
 	 * Form_Builder class
 	 */
 	Torro_Templatetags.prototype = {
-		init: function () {
+		init: function(){
 			this.init_templatetag_buttons();
 		},
 
 		/**
 		 * Handling the Templatetag Button
 		 */
-		init_templatetag_buttons: function () {
-			$('html').on('click', function () {
-				$('.torro-templatetag-list').hide();
-			});
+		init_templatetag_buttons: function(){
+			$( 'html' ).on( 'click', function(){
+				$( '.torro-templatetag-list' ).hide();
+			} );
 
-			$('.torro-templatetag-button').on('click', function (e) {
-				var $list = $(this).find('.torro-templatetag-list');
+			$( '.torro-templatetag-button' ).on( 'click', function( e ){
+				var $list = $( this ).find( '.torro-templatetag-list' );
 
-				if ('none' == $list.css('display')) {
+				if( 'none' == $list.css( 'display' ) ){
 					$list.show();
-				} else {
+				}else{
 					$list.hide();
 				}
 
 				e.stopPropagation();
-			});
+			} );
 
-			var $template_tag = $('.torro-templatetag-list .torro-templatetag');
+			var $template_tag = $( '.torro-templatetag-list .torro-templatetag' );
 
 			$template_tag.unbind();
 
-			$template_tag.on('click', function () {
-				var tag_name = '{' + $(this).attr('data-tagname') + '}';
-				var input_id = $(this).attr('data-input-id');
-				var editor = tinymce.get(input_id);
+			$template_tag.on( 'click', function(){
+				var tag_name = '{' + $( this ).attr( 'data-tagname' ) + '}';
+				var input_id = $( this ).attr( 'data-input-id' );
+				var editor = tinymce.get( input_id );
 
-				if (editor && editor instanceof tinymce.Editor) {
-					editor.insertContent(tag_name);
-				} else {
-					var $input = $('input[name="' + input_id + '"]');
-					$input.val($input.val() + tag_name);
+				if( editor && editor instanceof tinymce.Editor ){
+					editor.insertContent( tag_name );
+				}else{
+					var $input = $( 'input[name="' + input_id + '"]' );
+					$input.val( $input.val() + tag_name );
 				}
-			});
+			} );
 		},
 
-		rand: function () {
+		rand: function(){
 			var now = new Date();
-			var random = Math.floor(Math.random() * ( 10000 - 10 + 1 )) + 10;
+			var random = Math.floor( Math.random() * ( 10000 - 10 + 1 ) ) + 10;
 
 			random = random * now.getTime();
 			random = random.toString();
@@ -68,5 +66,5 @@
 		}
 	};
 
-	exports.add_extension('templatetags', new Torro_Templatetags(translations));
-}(form_builder, wp, jQuery, translation_fb) );
+	exports.add_extension( 'templatetags', new Torro_Templatetags( translations ) );
+}( form_builder, wp, jQuery, translation_fb ) );
