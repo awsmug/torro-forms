924c1ac39ac68e54656038da833697e32960fe24
Felix Arntz <felix-arntz@t-online.de>
2016-04-27 14:03:56 +0200

    added class PHPDoc for rest of core

--- core/ajax.php
+++ core/ajax.php
@@ -1,33 +1,24 @@
 <?php
 /**
- * The AJAX handler class.
+ * Core: Torro_AJAX class
  *
- * @author  awesome.ug, Author <support@awesome.ug>
- * @package TorroForms/Core
- * @version 1.0.0alpha1
- * @since   1.0.0
- * @license GPL 2
- *
- * Copyright 2015 awesome.ug (support@awesome.ug)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @package TorroForms
+ * @subpackage Core
+ * @version 1.0.0beta1
+ * @since 1.0.0beta1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Torro Forms AJAX handler class
+ *
+ * Handles all AJAX requests for the plugin and its extensions.
+ *
+ * @since 1.0.0beta1
+ */
 final class Torro_AJAX {
 	/**
 	 * Intance
--- core/templatetags/base-templatetags/global.php
+++ core/templatetags/base-templatetags/global.php
@@ -1,34 +1,24 @@
 <?php
-
 /**
- * Adds global Templatetags
+ * Core: Torro_TemplateTags_Global class
  *
- * @author  awesome.ug, Author <support@awesome.ug>
- * @package TorroForms/Core
- * @version 1.0.0alpha1
- * @since   1.0.0
- * @license GPL 2
- *
- * Copyright 2015 awesome.ug (support@awesome.ug)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @package TorroForms
+ * @subpackage CoreTemplateTags
+ * @version 1.0.0beta1
+ * @since 1.0.0beta1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Torro Forms global template tags handler class
+ *
+ * Handles template tags for global scope.
+ *
+ * @since 1.0.0beta1
+ */
 final class Torro_Templatetags_Global extends Torro_TemplateTags {
 	/**
 	 * Instance
