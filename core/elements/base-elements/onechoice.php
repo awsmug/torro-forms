8f9b6e1c1add11f81255f612b98b199bfd05d3f7
Felix Arntz <felix-arntz@t-online.de>
2016-04-27 13:50:32 +0200

    added class PHPDoc for elements

--- core/elements/base-elements/onechoice.php
+++ core/elements/base-elements/onechoice.php
@@ -1,33 +1,22 @@
 <?php
 /**
- * One Choice Form Element
+ * Core: Torro_Form_Element_Onechoice class
  *
- * @author  awesome.ug, Author <support@awesome.ug>
- * @package TorroForms/Core/Elements
- * @version 1.0.0alpha1
- * @since   1.0.0
- * @license GPL 2
- *
- * Copyright 2015 awesome.ug (support@awesome.ug)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * @package TorroForms
+ * @subpackage CoreElements
+ * @version 1.0.0beta1
+ * @since 1.0.0beta1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Element class for a one choice input
+ *
+ * @since 1.0.0beta1
+ */
 final class Torro_Form_Element_Onechoice extends Torro_Form_Element {
 	/**
 	 * Initializing.
