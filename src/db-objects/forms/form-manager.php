eedf32301d2fdd29ce00b0ae8552afd8886643e5
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-04-30 14:13:18 +0200

    Revert "Use status trait in form manager class."
    This reverts commit be835ab091cee8fd46ee5c07d244e0a7d39b80e3.

--- src/db-objects/forms/form-manager.php
+++ src/db-objects/forms/form-manager.php
@@ -12,7 +12,6 @@ use Leaves_And_Love\Plugin_Lib\DB_Objects\Managers\Core_Manager;
 use Leaves_And_Love\Plugin_Lib\DB_Objects\Traits\Title_Manager_Trait;
 use Leaves_And_Love\Plugin_Lib\DB_Objects\Traits\Slug_Manager_Trait;
 use Leaves_And_Love\Plugin_Lib\DB_Objects\Traits\Author_Manager_Trait;
-use Leaves_And_Love\Plugin_Lib\DB_Objects\Traits\Status_Manager_Trait;
 use Leaves_And_Love\Plugin_Lib\DB_Objects\Traits\Meta_Manager_Trait;
 use Leaves_And_Love\Plugin_Lib\DB_Objects\Traits\Capability_Manager_Trait;
 use Leaves_And_Love\Plugin_Lib\DB_Objects\Traits\REST_API_Manager_Trait;
@@ -30,7 +29,7 @@ use awsmug\Torro_Forms\DB_Objects\Manager_With_Children_Trait;
  * @method Leaves_And_Love\Plugin_Lib\Error_Handler              error_handler()
  */
 class Form_Manager extends Core_Manager {
-	use Title_Manager_Trait, Slug_Manager_Trait, Author_Manager_Trait, Status_Manager_Trait, Meta_Manager_Trait, Capability_Manager_Trait, REST_API_Manager_Trait, Manager_With_Children_Trait;
+	use Title_Manager_Trait, Slug_Manager_Trait, Author_Manager_Trait, Meta_Manager_Trait, Capability_Manager_Trait, REST_API_Manager_Trait, Manager_With_Children_Trait;
 
 	/**
 	 * Constructor.
@@ -69,7 +68,6 @@ class Form_Manager extends Core_Manager {
 		$this->title_property   = 'title';
 		$this->slug_property    = 'slug';
 		$this->author_property  = 'author';
-		$this->status_property  = 'status';
 
 		parent::__construct( $prefix, $services, $translations );
 	}
