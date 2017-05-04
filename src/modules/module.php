75b4062177fa8a5581098599a584d7a3920b8f3b
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-05-04 15:45:40 +0200

    Make it easy to handle module settings.

--- src/modules/module.php
+++ src/modules/module.php
@@ -135,10 +135,137 @@ abstract class Module extends Service {
 	}
 
 	/**
+	 * Retrieves the value of a specific module option.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param string $option  Name of the option to retrieve.
+	 * @param mixed  $default Optional. Value to return if the option doesn't exist. Default false.
+	 * @return mixed Value set for the option.
+	 */
+	public function get_option( $option, $default = false ) {
+		$options = $this->manager()->options()->get( $this->get_settings_identifier(), array() );
+
+		if ( isset( $options[ $option ] ) ) {
+			return $options[ $option ];
+		}
+
+		return $default;
+	}
+
+	/**
+	 * Adds settings subtabs, sections and fields for the module to the plugin settings page.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param awsmug\Torro_Forms\DB_Objects\Forms\Form_Settings_Page $settings_page Settings page instance.
+	 */
+	protected final function add_settings( $settings_page ) {
+		$subtabs = $this->get_settings_subtabs();
+		if ( empty( $subtabs ) ) {
+			return;
+		}
+
+		$sections = $this->get_settings_sections();
+		$fields   = $this->get_settings_fields();
+
+		$tab_id = $this->get_settings_identifier();
+
+		$settings_page->add_tab( $tab_id, array(
+			'title' => $this->get_title(),
+		) );
+
+		foreach ( $subtabs as $slug => $args ) {
+			$args['tab'] = $tab_id;
+
+			$settings_page->add_subtab( $slug, $args );
+		}
+
+		foreach ( $sections as $slug => $args ) {
+			$settings_page->add_section( $slug, $args );
+		}
+
+		foreach ( $fields as $slug => $args ) {
+			$type = 'text';
+			if ( isset( $args['type'] ) ) {
+				$type = $args['type'];
+				unset( $args['type'] );
+			}
+
+			$settings_page->add_field( $slug, $type, $args );
+		}
+	}
+
+	/**
+	 * Returns the settings identifier for the module.
+	 *
+	 * This identifier must be used to access module options.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return string Module settings identifier.
+	 */
+	protected final function get_settings_identifier() {
+		return 'module_' . $this->slug;
+	}
+
+	/**
+	 * Sets up all action and filter hooks for the service.
+	 *
+	 * This method must be implemented and then be called from the constructor.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function setup_hooks() {
+		$this->actions = array(
+			array(
+				'name'     => "{$this->manager->get_prefix()}add_form_settings_content",
+				'callback' => array( $this, 'add_settings' ),
+				'priority' => 1,
+				'num_args' => 1,
+			),
+		);
+	}
+
+	/**
 	 * Bootstraps the module by setting properties.
 	 *
 	 * @since 1.0.0
 	 * @access protected
 	 */
 	protected abstract function bootstrap();
+
+	/**
+	 * Returns the available settings sub-tabs for the module.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return array Associative array of `$subtab_slug => $subtab_args` pairs.
+	 */
+	protected abstract function get_settings_subtabs();
+
+	/**
+	 * Returns the available settings sections for the module.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return array Associative array of `$section_slug => $section_args` pairs.
+	 */
+	protected abstract function get_settings_sections();
+
+	/**
+	 * Returns the available settings fields for the module.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return array Associative array of `$field_slug => $field_args` pairs.
+	 */
+	protected abstract function get_settings_fields();
 }
