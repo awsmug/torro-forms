2019881e6eb0f323b76e5f9459ac629b25e6641a
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-05-15 23:20:33 +0200

    Improve element type field sanitization and trigger error notices.

--- src/db-objects/elements/element-types/element-type.php
+++ src/db-objects/elements/element-types/element-type.php
@@ -8,6 +8,8 @@
 
 namespace awsmug\Torro_Forms\DB_Objects\Elements\Element_Types;
 
+use Leaves_And_Love\Plugin_Lib\Fields\Field_Manager;
+
 /**
  * Base class representing an element type.
  *
@@ -231,6 +233,13 @@ abstract class Element_Type {
 
 		foreach ( $this->settings_fields as $slug => $field ) {
 			if ( empty( $field['section'] ) || ! isset( $this->settings_sections[ $field['section'] ] ) ) {
+				$this->manager->error_handler()->doing_it_wrong( get_class( $this ) . '::bootstrap()', sprintf( __( 'Invalid element type field section %s.', 'torro-forms' ), esc_html( $field['section'] ) ), '1.0.0' );
+				$invalid_fields[ $slug ] = true;
+				continue;
+			}
+
+			if ( empty( $field['type'] ) || ! Field_Manager::is_field_type_registered( $field['type'] ) ) {
+				$this->manager->error_handler()->doing_it_wrong( get_class( $this ) . '::bootstrap()', sprintf( __( 'Invalid element type field type %s.', 'torro-forms' ), esc_html( $field['type'] ) ), '1.0.0' );
 				$invalid_fields[ $slug ] = true;
 				continue;
 			}
