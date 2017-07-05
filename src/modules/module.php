c3ef3a1364ea6b97bb6fa4211934f2f0ef5e706f
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-07-05 17:56:03 +0200

    Scaffold module meta functionality.

--- src/modules/module.php
+++ src/modules/module.php
@@ -8,6 +8,7 @@
 
 namespace awsmug\Torro_Forms\Modules;
 
+use awsmug\Torro_Forms\DB_Objects\Forms\Form_Edit_Page_Handler;
 use awsmug\Torro_Forms\DB_Objects\Forms\Form_Settings_Page;
 use Leaves_And_Love\Plugin_Lib\Service;
 use Leaves_And_Love\Plugin_Lib\Traits\Container_Service_Trait;
@@ -137,6 +138,27 @@ abstract class Module extends Service {
 	}
 
 	/**
+	 * Retrieves the value of a specific module option for a specific form.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param int    $form_id Form ID.
+	 * @param string $option  Name of the form option to retrieve.
+	 * @param mixed  $default Optional. Value to return if the form option doesn't exist. Default false.
+	 * @return mixed Value set for the option.
+	 */
+	public function get_form_option( $form_id, $option, $default = false ) {
+		$metadata = $this->manager()->meta()->get( 'post', $form_id, $this->get_meta_identifier(), true );
+
+		if ( is_array( $metadata ) && isset( $metadata[ $option ] ) ) {
+			return $metadata[ $option ];
+		}
+
+		return $default;
+	}
+
+	/**
 	 * Retrieves the value of a specific module option.
 	 *
 	 * @since 1.0.0
@@ -154,6 +176,66 @@ abstract class Module extends Service {
 		}
 
 		return $default;
+	}
+
+	/**
+	 * Adds meta tabs, sections and fields for the module to the form edit page.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param Form_Edit_Page_Handler $settings_page Settings page instance.
+	 */
+	protected final function add_meta( $edit_page ) {
+		$tabs = $this->get_meta_tabs();
+		if ( empty( $tabs ) ) {
+			return;
+		}
+
+		$sections = $this->get_meta_sections();
+		$fields   = $this->get_meta_fields();
+
+		$meta_box_id = $this->get_meta_identifier();
+
+		$edit_page->add_meta_box( $meta_box_id, array(
+			'title'    => $this->get_title(),
+			'context'  => 'normal',
+			'priority' => 'high',
+		) );
+
+		foreach ( $tabs as $slug => $args ) {
+			$args['meta_box'] = $meta_box_id;
+
+			$edit_page->add_tab( $slug, $args );
+		}
+
+		foreach ( $sections as $slug => $args ) {
+			$edit_page->add_section( $slug, $args );
+		}
+
+		foreach ( $fields as $slug => $args ) {
+			$type = 'text';
+			if ( isset( $args['type'] ) ) {
+				$type = $args['type'];
+				unset( $args['type'] );
+			}
+
+			$edit_page->add_field( $slug, $type, $args );
+		}
+	}
+
+	/**
+	 * Returns the meta identifier for the module.
+	 *
+	 * This identifier must be used to access module metadata.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return string Module meta identifier.
+	 */
+	protected final function get_meta_identifier() {
+		return 'module_' . $this->slug;
 	}
 
 	/**
@@ -225,6 +307,12 @@ abstract class Module extends Service {
 	protected function setup_hooks() {
 		$this->actions = array(
 			array(
+				'name'     => "{$this->get_prefix()}add_form_meta_content",
+				'callback' => array( $this, 'add_meta' ),
+				'priority' => 1,
+				'num_args' => 1,
+			),
+			array(
 				'name'     => "{$this->get_prefix()}add_settings_content",
 				'callback' => array( $this, 'add_settings' ),
 				'priority' => 1,
@@ -246,6 +334,36 @@ abstract class Module extends Service {
 	 * @access protected
 	 */
 	protected abstract function bootstrap();
+
+	/**
+	 * Returns the available meta box tabs for the module.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return array Associative array of `$tab_slug => $tab_args` pairs.
+	 */
+	protected abstract function get_meta_tabs();
+
+	/**
+	 * Returns the available meta box sections for the module.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return array Associative array of `$section_slug => $section_args` pairs.
+	 */
+	protected abstract function get_meta_sections();
+
+	/**
+	 * Returns the available meta box fields for the module.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return array Associative array of `$field_slug => $field_args` pairs.
+	 */
+	protected abstract function get_meta_fields();
 
 	/**
 	 * Returns the available settings sub-tabs for the module.
