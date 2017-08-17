be5f0614e18ebdccec7e657ffbbe1b9ee6f40cf4
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-08-17 14:51:08 +0200

    Implement form settings module.

--- src/modules/form-settings/module.php
+++ src/modules/form-settings/module.php
@@ -9,11 +9,6 @@
 namespace awsmug\Torro_Forms\Modules\Form_Settings;
 
 use awsmug\Torro_Forms\Modules\Module as Module_Base;
-use awsmug\Torro_Forms\Modules\Submodule_Registry_Interface;
-use awsmug\Torro_Forms\Modules\Submodule_Registry_Trait;
-use awsmug\Torro_Forms\Modules\Meta_Submodule_Interface;
-use awsmug\Torro_Forms\Modules\Settings_Submodule_Interface;
-use awsmug\Torro_Forms\Modules\Assets_Submodule_Interface;
 use awsmug\Torro_Forms\Assets;
 
 /**
@@ -21,8 +16,7 @@ use awsmug\Torro_Forms\Assets;
  *
  * @since 1.0.0
  */
-class Module extends Module_Base implements Submodule_Registry_Interface {
-	use Submodule_Registry_Trait;
+class Module extends Module_Base {
 
 	/**
 	 * Bootstraps the module by setting properties.
@@ -34,34 +28,6 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 		$this->slug        = 'form_settings';
 		$this->title       = __( 'Form Settings', 'torro-forms' );
 		$this->description = __( 'Form settings control the general behavior of forms.', 'torro-forms' );
-
-		$this->submodule_base_class = Form_Setting::class;
-		// TODO: Setup $default_submodules.
-	}
-
-	/**
-	 * Registers the default form settings.
-	 *
-	 * The function also executes a hook that should be used by other developers to register their own form settings.
-	 *
-	 * @since 1.0.0
-	 * @access protected
-	 */
-	protected function register_defaults() {
-		foreach ( $this->default_submodules as $slug => $form_setting_class_name ) {
-			$this->register( $slug, $form_setting_class_name );
-		}
-
-		/**
-		 * Fires when the default form settings have been registered.
-		 *
-		 * This action should be used to register custom form settings.
-		 *
-		 * @since 1.0.0
-		 *
-		 * @param Module $form_settings Form setting manager instance.
-		 */
-		do_action( "{$this->get_prefix()}register_form_settings", $this );
 	}
 
 	/**
@@ -73,26 +39,27 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @return array Associative array of `$tab_slug => $tab_args` pairs.
 	 */
 	protected function get_meta_tabs() {
-		$tabs = array();
+		$prefix = $this->manager()->get_prefix();
 
-		foreach ( $this->submodules as $slug => $form_setting ) {
-			if ( ! is_a( $form_setting, Meta_Submodule_Interface::class ) ) {
-				continue;
-			}
+		$tabs = array(
+			'labels'   => array(
+				'title'       => _x( 'Labels', 'form settings tab', 'torro-forms' ),
+				'description' => __( 'Allows you to tweak some labels and messages displayed in the form frontend.', 'torro-form' ),
+			),
+			'advanced' => array(
+				'title'       => _x( 'Advanced', 'form settings tab', 'torro-forms' ),
+				'description' => __( 'Allows you to modify advanced form settings.', 'torro-forms' ),
+			),
+		);
 
-			$form_setting_meta_identifier = $form_setting->get_meta_identifier();
-			$form_setting_meta_fields = $form_setting->get_meta_fields();
-			if ( empty( $form_setting_meta_fields ) ) {
-				continue;
-			}
-
-			$tabs[ $form_setting_meta_identifier ] = array(
-				'title'       => $form_setting->get_meta_title(),
-				'description' => $form_setting->get_meta_description(),
-			);
-		}
-
-		return $tabs;
+		/**
+		 * Filters the meta tabs in the form settings metabox.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $tabs Array of `$tab_slug => $tab_data` pairs.
+		 */
+		return apply_filters( "{$prefix}form_settings_meta_tabs", $tabs );
 	}
 
 	/**
@@ -104,39 +71,64 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @return array Associative array of `$field_slug => $field_args` pairs.
 	 */
 	protected function get_meta_fields() {
-		$fields = array();
+		$prefix = $this->manager()->get_prefix();
 
-		foreach ( $this->submodules as $slug => $form_setting ) {
-			if ( ! is_a( $form_setting, Meta_Submodule_Interface::class ) ) {
-				continue;
-			}
+		$fields = array(
+			'show_container_title'  => array(
+				'tab'         => 'labels',
+				'type'        => 'checkbox',
+				'label'       => __( 'Show container title?', 'torro-forms' ),
+				'description' => __( 'Click the checkbox to display the title of the current container in the frontend.', 'torro-forms' ),
+				'default'     => true,
+			),
+			'previous_button_label' => array(
+				'tab'         => 'labels',
+				'type'        => 'text',
+				'label'       => __( 'Previous Button Label', 'torro-forms' ),
+				'description' => __( 'Enter the label for the button that leads to the previous form page.', 'torro-forms' ),
+				'default'     => $this->get_default_previous_button_label(),
+			),
+			'next_button_label'     => array(
+				'tab'         => 'labels',
+				'type'        => 'text',
+				'label'       => __( 'Next Button Label', 'torro-forms' ),
+				'description' => __( 'Enter the label for the button that leads to the next form page.', 'torro-forms' ),
+				'default'     => $this->get_default_next_button_label(),
+			),
+			'submit_button_label'   => array(
+				'tab'         => 'labels',
+				'type'        => 'text',
+				'label'       => __( 'Submit Button Label', 'torro-forms' ),
+				'description' => __( 'Enter the label for the button that submits the form.', 'torro-forms' ),
+				'default'     => $this->get_default_submit_button_label(),
+			),
+			'success_message'       => array(
+				'tab'           => 'labels',
+				'type'          => 'text',
+				'label'         => __( 'Success Message', 'torro-forms' ),
+				'description'   => __( 'Enter a message to display when a form submission has successfully been completed.', 'torro-forms' ),
+				'input_classes' => array( 'regular-text' ),
+				'default'       => $this->get_default_success_message(),
+			),
+			'allow_get_params'      => array(
+				'tab'         => 'advanced',
+				'type'        => 'checkbox',
+				'label'       => __( 'Allow GET parameters?', 'torro-forms' ),
+				/* translators: %s: GET parameter example */
+				'description' => sprintf( __( 'Click the checkbox to allow initial field values to be set through GET parameters (such as %s).', 'torro-forms' ), '<code>?torro_input_value_ELEMENT_ID=VALUE</code>' ),
+			),
+		);
 
-			$form_setting_meta_identifier = $form_setting->get_meta_identifier();
+		// TODO: backward-compatibility with 'show_page_title' ('yes' or 'no'), 'previous_button_text', 'next_button_text', 'send_button_text', 'redirect_text_content' and 'allow_get_param' ('yes' or 'no')
 
-			$form_setting_meta_fields = $form_setting->get_meta_fields();
-			foreach ( $form_setting_meta_fields as $field_slug => $field_data ) {
-				$field_slug        = $form_setting_meta_identifier . '__' . $field_slug;
-				$field_data['tab'] = $form_setting_meta_identifier;
-
-				if ( isset( $field_data['dependencies'] ) ) {
-					$dependency_count = count( $field_data['dependencies'] );
-
-					for ( $i = 0; $i < $dependency_count; $i++ ) {
-						if ( isset( $field_data['dependencies'][ $i ]['fields'] ) ) {
-							$field_count = count( $field_data['dependencies'][ $i ]['fields'] );
-
-							for ( $j = 0; $j < $field_count; $j++ ) {
-								$field_data['dependencies'][ $i ]['fields'][ $j ] = $form_setting_meta_identifier . '__' . $field_data['dependencies'][ $i ]['fields'][ $j ];
-							}
-						}
-					}
-				}
-
-				$fields[ $field_slug ] = $field_data;
-			}
-		}
-
-		return $fields;
+		/**
+		 * Filters the meta fields in the form settings metabox.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $fields Array of `$field_slug => $field_data` pairs.
+		 */
+		return apply_filters( "{$prefix}form_settings_meta_fields", $fields );
 	}
 
 	/**
@@ -148,25 +140,18 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @return array Associative array of `$subtab_slug => $subtab_args` pairs.
 	 */
 	protected function get_settings_subtabs() {
+		$prefix = $this->manager()->get_prefix();
+
 		$subtabs = array();
 
-		foreach ( $this->submodules as $slug => $form_setting ) {
-			if ( ! is_a( $form_setting, Settings_Submodule_Interface::class ) ) {
-				continue;
-			}
-
-			$form_setting_settings_identifier = $form_setting->get_settings_identifier();
-			$form_setting_settings_sections = $form_setting->get_settings_sections();
-			if ( empty( $form_setting_settings_sections ) ) {
-				continue;
-			}
-
-			$subtabs[ $form_setting_settings_identifier ] = array(
-				'title' => $form_setting->get_settings_title(),
-			);
-		}
-
-		return $subtabs;
+		/**
+		 * Filters the settings subtabs in the form settings tab.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $fields Array of `$subtab_slug => $subtab_data` pairs.
+		 */
+		return apply_filters( "{$prefix}form_settings_settings_subtabs", $subtabs );
 	}
 
 	/**
@@ -178,24 +163,18 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @return array Associative array of `$section_slug => $section_args` pairs.
 	 */
 	protected function get_settings_sections() {
+		$prefix = $this->manager()->get_prefix();
+
 		$sections = array();
 
-		foreach ( $this->submodules as $slug => $form_setting ) {
-			if ( ! is_a( $form_setting, Settings_Submodule_Interface::class ) ) {
-				continue;
-			}
-
-			$form_setting_settings_identifier = $form_setting->get_settings_identifier();
-
-			$form_setting_settings_sections = $form_setting->get_settings_sections();
-			foreach ( $form_setting_settings_sections as $section_slug => $section_data ) {
-				$section_data['subtab'] = $form_setting_settings_identifier;
-
-				$sections[ $section_slug ] = $section_data;
-			}
-		}
-
-		return $sections;
+		/**
+		 * Filters the settings sections in the form settings tab.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $fields Array of `$section_slug => $section_data` pairs.
+		 */
+		return apply_filters( "{$prefix}form_settings_settings_sections", $sections );
 	}
 
 	/**
@@ -207,17 +186,18 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @return array Associative array of `$field_slug => $field_args` pairs.
 	 */
 	protected function get_settings_fields() {
+		$prefix = $this->manager()->get_prefix();
+
 		$fields = array();
 
-		foreach ( $this->submodules as $slug => $form_setting ) {
-			if ( ! is_a( $form_setting, Settings_Submodule_Interface::class ) ) {
-				continue;
-			}
-
-			$fields = array_merge( $fields, $form_setting->get_settings_fields() );
-		}
-
-		return $fields;
+		/**
+		 * Filters the settings fields in the form settings tab.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $fields Array of `$field_slug => $field_data` pairs.
+		 */
+		return apply_filters( "{$prefix}form_settings_settings_fields", $fields );
 	}
 
 	/**
@@ -229,13 +209,16 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @param Assets $assets Assets API instance.
 	 */
 	protected function register_assets( $assets ) {
-		foreach ( $this->submodules as $slug => $form_setting ) {
-			if ( ! is_a( $form_setting, Assets_Submodule_Interface::class ) ) {
-				continue;
-			}
+		$prefix = $this->manager()->get_prefix();
 
-			$form_setting->register_assets( $assets );
-		}
+		/**
+		 * Fires when form settings assets should be registered.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param Assets $assets The plugin assets instance.
+		 */
+		do_action( "{$prefix}form_settings_register_assets", $assets );
 	}
 
 	/**
@@ -247,13 +230,169 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @param Assets $assets Assets API instance.
 	 */
 	protected function enqueue_form_builder_assets( $assets ) {
-		foreach ( $this->submodules as $slug => $form_setting ) {
-			if ( ! is_a( $form_setting, Assets_Submodule_Interface::class ) ) {
-				continue;
-			}
+		$prefix = $this->manager()->get_prefix();
 
-			$form_setting->enqueue_form_builder_assets( $assets );
+		/**
+		 * Fires when form settings assets for the form builder should be enqueued.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param Assets $assets The plugin assets instance.
+		 */
+		do_action( "{$prefix}form_settings_enqueue_form_builder_assets", $assets );
+	}
+
+	/**
+	 * Returns the default label for the Previous button.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return string Message to display.
+	 */
+	protected function get_default_previous_button_label() {
+		return _x( 'Previous Step', 'button label', 'torro-forms' );
+	}
+
+	/**
+	 * Returns the default label for the Next button.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return string Message to display.
+	 */
+	protected function get_default_next_button_label() {
+		return _x( 'Next Step', 'button label', 'torro-forms' );
+	}
+
+	/**
+	 * Returns the default label for the Submit button.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return string Message to display.
+	 */
+	protected function get_default_submit_button_label() {
+		return _x( 'Submit', 'button label', 'torro-forms' );
+	}
+
+	/**
+	 * Returns the default message to display when a form submission has been completed.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @return string Message to display.
+	 */
+	protected function get_default_success_message() {
+		return __( 'Thank you for submitting!', 'torro-forms' );
+	}
+
+	/**
+	 * Filters whether to show the title of the current container in the frontend.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param bool $show_container_title Whether to show the title.
+	 * @param int  $form_id              Form ID.
+	 * @return bool True or false depending on the form setting.
+	 */
+	protected function filter_show_container_title( $show_container_title, $form_id ) {
+		return (bool) $this->get_form_option( $form_id, 'show_container_title', true );
+	}
+
+	/**
+	 * Filters the label for the Previous button in the frontend.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param string $prev_button_label The Previous button label.
+	 * @param int    $form_id           Form ID.
+	 * @return string The Previous button label depending on the form setting.
+	 */
+	protected function filter_previous_button_label( $prev_button_label, $form_id ) {
+		$prev_button_label = $this->get_form_option( $form_id, 'previous_button_label', '' );
+		if ( empty( $prev_button_label ) ) {
+			$prev_button_label = $this->get_default_previous_button_label();
 		}
+
+		return $prev_button_label;
+	}
+
+	/**
+	 * Filters the label for the Next button in the frontend.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param string $next_button_label The Next button label.
+	 * @param int    $form_id           Form ID.
+	 * @return string The Next button label depending on the form setting.
+	 */
+	protected function filter_next_button_label( $next_button_label, $form_id ) {
+		$next_button_label = $this->get_form_option( $form_id, 'next_button_label', '' );
+		if ( empty( $next_button_label ) ) {
+			$next_button_label = $this->get_default_next_button_label();
+		}
+
+		return $next_button_label;
+	}
+
+	/**
+	 * Filters the label for the Submit button in the frontend.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param string $submit_button_label The Submit button label.
+	 * @param int    $form_id             Form ID.
+	 * @return string The Submit button label depending on the form setting.
+	 */
+	protected function filter_submit_button_label( $submit_button_label, $form_id ) {
+		$submit_button_label = $this->get_form_option( $form_id, 'submit_button_label', '' );
+		if ( empty( $submit_button_label ) ) {
+			$submit_button_label = $this->get_default_submit_button_label();
+		}
+
+		return $submit_button_label;
+	}
+
+	/**
+	 * Filters the success message for a completed form submission in the frontend.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param string $success_message The success message.
+	 * @param int    $form_id         Form ID.
+	 * @return string The success message depending on the form setting.
+	 */
+	protected function filter_success_message( $success_message, $form_id ) {
+		$success_message = $this->get_form_option( $form_id, 'success_message', '' );
+		if ( empty( $success_message ) ) {
+			$success_message = $this->get_default_success_message();
+		}
+
+		return $success_message;
+	}
+
+	/**
+	 * Filters whether to allow GET parameters to pre-populate form element values.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param bool $allow_get_params Whether to allow GET parameters.
+	 * @param int  $element_id       Element ID.
+	 * @param int  $form_id          Form ID.
+	 * @return bool True or false depending on the form setting.
+	 */
+	protected function filter_allow_get_params( $allow_get_params, $element_id, $form_id ) {
+		return (bool) $this->get_form_option( $form_id, 'allow_get_params' );
 	}
 
 	/**
@@ -265,11 +404,43 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	protected function setup_hooks() {
 		parent::setup_hooks();
 
-		$this->actions[] = array(
-			'name'     => 'init',
-			'callback' => array( $this, 'register_defaults' ),
-			'priority' => 100,
-			'num_args' => 1,
+		$prefix = $this->get_prefix();
+
+		$this->filters[] = array(
+			'name'     => "{$prefix}form_container_show_title",
+			'callback' => array( $this, 'filter_show_container_title' ),
+			'priority' => 10,
+			'num_args' => 2,
+		);
+		$this->filters[] = array(
+			'name'     => "{$prefix}form_button_prev_step_label",
+			'callback' => array( $this, 'filter_previous_button_label' ),
+			'priority' => 10,
+			'num_args' => 2,
+		);
+		$this->filters[] = array(
+			'name'     => "{$prefix}form_button_next_step_label",
+			'callback' => array( $this, 'filter_next_button_label' ),
+			'priority' => 10,
+			'num_args' => 2,
+		);
+		$this->filters[] = array(
+			'name'     => "{$prefix}form_button_submit_label",
+			'callback' => array( $this, 'filter_submit_button_label' ),
+			'priority' => 10,
+			'num_args' => 2,
+		);
+		$this->filters[] = array(
+			'name'     => "{$prefix}form_submission_success_message",
+			'callback' => array( $this, 'filter_success_message' ),
+			'priority' => 10,
+			'num_args' => 2,
+		);
+		$this->filters[] = array(
+			'name'     => "{$prefix}allow_get_params",
+			'callback' => array( $this, 'filter_allow_get_params' ),
+			'priority' => 10,
+			'num_args' => 3,
 		);
 	}
 }
