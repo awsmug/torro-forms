912bcff8c29ed57a0bae2edb02c2b1ee5e3db9ea
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-08-25 15:36:58 +0200

    Implement form upload manager.

--- src/components/form-upload-manager.php
+++ src/components/form-upload-manager.php
@@ -11,6 +11,9 @@ namespace awsmug\Torro_Forms\Components;
 use Leaves_And_Love\Plugin_Lib\Service;
 use Leaves_And_Love\Plugin_Lib\Traits\Container_Service_Trait;
 use awsmug\Torro_Forms\DB_Objects\Taxonomy_Manager;
+use awsmug\Torro_Forms\DB_Objects\Forms\Form;
+use awsmug\Torro_Forms\DB_Objects\Submissions\Submission;
+use WP_Error;
 
 /**
  * Class for managing media files to upload for a form submission.
@@ -50,10 +53,195 @@ class Form_Upload_Manager extends Service {
 	}
 
 	/**
-	 * Checks whether the typical image sizes should be generated for form uploads.
+	 * Uploads a new file for a specific form submission and element.
 	 *
-	 * By default this will be skipped. A constant 'TORRO_CREATE_FORM_UPLOAD_IMAGE_SIZES' can be used
-	 * to tweak it.
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param Submission $submission       Submission object.
+	 * @param Form       $form             Form object.
+	 * @param int        $element_id       Element ID.
+	 * @param string     $field            Optional. Element field slug. Default is '_main'.
+	 * @param array      $allowed_mimes    Optional. Allowed MIME types. Default are all MIME types that WordPress core allows.
+	 * @param int        $allowed_filesize Optional. Allowed maximum file size. Default is no limit other than WordPress core restrictions.
+	 * @return int|WP_Error Attachment ID for the new file, or error object on failure.
+	 */
+	public function upload_file( $file_id, $submission, $form, $element_id, $field = '_main', $allowed_mimes = null, $allowed_filesize = null ) {
+		if ( ! isset( $_FILES[ $file_id ] ) ) {
+			return new WP_Error( 'missing_file', __( 'No file was provided to upload.', 'torro-forms' ) );
+		}
+
+		$prefix = $this->get_prefix();
+
+		if ( ! $field ) {
+			$field = '_main';
+		}
+
+		$attachment_data = array(
+			'post_title' => sprintf( __( 'Form upload for submission #%1$s (form &#8220;%2$s&#8221;)', 'torro-forms' ), $submission->id, $form->title ),
+			'meta_input' => array(
+				$prefix . 'parent_submission_id' => $submission->id,
+				$prefix . 'parent_form_id'       => $form->id,
+				$prefix . 'parent_element_id'    => $element->id,
+				$prefix . 'parent_element_field' => $field,
+			),
+		);
+
+		$post_id = 0;
+		if ( $this->should_set_parent_form( $form->id ) ) {
+			$post_id = $form->id;
+		}
+
+		$overrides = array(
+			'mimes'     => $allowed_mimes,
+			'test_form'	=> false,
+			'test_type'	=> true,
+			'test_size'	=> true,
+		);
+
+		if ( $allowed_filesize ) {
+			$filesize = isset( $_FILES[ $file_id ]['size'] ) ? $_FILES[ $file_id ]['size'] : filesize( $_FILES[ $file_id ]['tmp_name'] );
+			if ( (int) $filesize > (int) $allowed_filesize ) {
+				return new WP_Error( 'upload_error', __( 'The file exceeds the maximum allowed size.', 'torro-forms' ) );
+			}
+		}
+
+		if ( ! function_exists( 'wp_handle_upload' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/file.php';
+		}
+		if ( ! function_exists( 'wp_read_image_metadata' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/image.php';
+		}
+		if ( ! function_exists( 'media_handle_upload' ) ) {
+			require_once ABSPATH . 'wp-admin/includes/media.php';
+		}
+
+		$added_filter = false;
+		if ( ! $this->should_generate_image_sizes( $form->id ) ) {
+			$added_filter = true;
+			add_filter( 'intermediate_image_sizes_advanced', '__return_empty_array', 9999 );
+		}
+
+		$attachment_id = media_handle_upload( $file_id, $post_id, $attachment_data, $overrides );
+
+		if ( $added_filter ) {
+			remove_filter( 'intermediate_image_sizes_advanced', '__return_empty_array', 9999 );
+		}
+
+		if ( is_wp_error( $attachment_id ) ) {
+			// The following line has no textdomain on purpose as it's a WP core message.
+			if ( $allowed_mimes && 'upload_error' === $attachment_id->get_error_code() && __( 'Sorry, this file type is not permitted for security reasons.' ) === $attachment_id->get_error_message() ) {
+				return new WP_Error( 'upload_error', __( 'The file type is not permitted.', 'torro-forms' ) );
+			}
+
+			return $attachment_id;
+		}
+
+		if ( ! $attachment_id ) {
+			return new WP_Error( 'upload_error', __( 'The file could not be registered with the database.', 'torro-forms' ) );
+		}
+
+		$attachment_id = (int) $attachment_id;
+
+		// Set the default attachment taxonomy term for form uploads.
+		$taxonomy_slug = $this->taxonomies()->get_attachment_taxonomy_slug();
+		if ( ! empty( $taxonomy_slug ) ) {
+			$taxonomy_term_id = $this->taxonomies()->get_attachment_taxonomy_term_id();
+			if ( ! empty( $taxonomy_term_id ) ) {
+				wp_set_post_terms( $attachment_id, array( $taxonomy_term_id ), $taxonomy_slug );
+			}
+		}
+
+		return $attachment_id;
+	}
+
+	/**
+	 * Deletes old files for a specific form submission and element.
+	 *
+	 * This should be used when a user uploads a new file for a field, to prevent database
+	 * clutter from the now unneeded files previously uploaded.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 *
+	 * @param Submission $submission Submission object.
+	 * @param Form       $form       Form object.
+	 * @param int        $element_id Element ID.
+	 * @param string     $field      Optional. Element field slug. Default is '_main'.
+	 * @return array Array where each element is either the deleted attachment ID, or an error object indicating a deletion failure.
+	 *               May also be empty in case nothing needed to be deleted.
+	 */
+	public function delete_old_files( $submission, $form, $element_id, $field = '_main' ) {
+		$prefix = $this->get_prefix();
+
+		if ( ! $field ) {
+			$field = '_main';
+		}
+
+		$attachment_ids = get_posts( array(
+			'fields'         => 'ids',
+			'posts_per_page' => 20,
+			'no_found_rows'  => true,
+			'post_type'      => 'attachment',
+			'post_status'    => 'inherit',
+			'meta_query'     => array(
+				'relation' => 'AND',
+				array(
+					'key'   => $prefix . 'parent_submission_id',
+					'value' => $submission->id,
+					'type'  => 'UNSIGNED',
+				),
+				array(
+					'key'   => $prefix . 'parent_element_id',
+					'value' => $element->id,
+					'type'  => 'UNSIGNED',
+				),
+				array(
+					'key'   => $prefix . 'parent_element_field',
+					'value' => $field,
+					'type'  => 'CHAR',
+				),
+			),
+		) );
+
+		$result = array();
+		foreach ( $attachment_ids as $attachment_id ) {
+			$post = wp_delete_attachment( $attachment_id, true );
+			if ( ! $post ) {
+				$result[] = new WP_Error( 'delete_error', __( 'The file could not be deleted.', 'torro-forms' ) );
+			} else {
+				$result[] = $post->ID;
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Checks whether form uploads should have the form they're uploaded for set as their parent.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 *
+	 * @param int $form_id Form ID for which to check this.
+	 * @return bool True if the form should be set as parent, false otherwise.
+	 */
+	protected function should_set_parent_form( $form_id ) {
+		$result = false;
+
+		/**
+		 * Filters whether form uploads should have the form they're uploaded for set as their parent.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param bool $result  True if the form should be set as parent, false otherwise. Default false.
+		 * @param int  $form_id Form ID for which to check this.
+		 */
+		return apply_filters( "{$this->get_prefix()}form_uploads_should_set_parent_form", $result, $form_id );
+	}
+
+	/**
+	 * Checks whether the typical image sizes should be generated for form uploads.
 	 *
 	 * @since 1.0.0
 	 * @access protected
@@ -64,16 +252,12 @@ class Form_Upload_Manager extends Service {
 	protected function should_generate_image_sizes( $form_id ) {
 		$result = false;
 
-		if ( defined( 'TORRO_CREATE_FORM_UPLOAD_IMAGE_SIZES' ) && TORRO_CREATE_FORM_UPLOAD_IMAGE_SIZES ) {
-			$result = true;
-		}
-
 		/**
 		 * Filters whether the typical image sizes should be generated for form upload images.
 		 *
 		 * @since 1.0.0
 		 *
-		 * @param bool $result  True if image sizes should be generated, false otherwise. Default depends on the constant.
+		 * @param bool $result  True if image sizes should be generated, false otherwise. Default false.
 		 * @param int  $form_id Form ID for which to check this.
 		 */
 		return apply_filters( "{$this->get_prefix()}form_uploads_should_generate_image_sizes", $result, $form_id );
