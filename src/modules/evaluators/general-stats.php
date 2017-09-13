2520e4f374aec91c27033598f40993f3b654ed8e
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-09-13 18:20:06 +0200

    Finish implementation of generalstats submodule.

--- src/modules/evaluators/general-stats.php
+++ src/modules/evaluators/general-stats.php
@@ -48,7 +48,31 @@ class General_Stats extends Evaluator implements Assets_Submodule_Interface {
 	 * @return array Updated aggregate evaluation results.
 	 */
 	public function evaluate_single( $aggregate_results, $submission, $form ) {
-		// TODO.
+		if ( ! isset( $aggregate_results['total'] ) ) {
+			$aggregate_results['total'] = 1;
+		} else {
+			$aggregate_results['total']++;
+		}
+
+		$year  = (string) $submission->format_datetime( 'Y' );
+		$month = (string) $submission->format_datetime( 'm' );
+
+		if ( ! isset( $aggregate_results[ $year ] ) ) {
+			$aggregate_results[ $year ] = array();
+		}
+
+		if ( ! isset( $aggregate_results[ $year ]['total'] ) ) {
+			$aggregate_results[ $year ]['total'] = 1;
+		} else {
+			$aggregate_results[ $year ]['total']++;
+		}
+
+		if ( ! isset( $aggregate_results[ $year ][ $month ] ) ) {
+			$aggregate_results[ $year ][ $month ] = 1;
+		} else {
+			$aggregate_results[ $year ][ $month ]++;
+		}
+
 		return $aggregate_results;
 	}
 
@@ -63,7 +87,89 @@ class General_Stats extends Evaluator implements Assets_Submodule_Interface {
 	 * @param array $args    Arguments to tweak the displayed results.
 	 */
 	public function show_results( $results, $form, $args = array() ) {
-		// TODO.
+		$total_count = 0;
+		if ( isset( $results['total'] ) ) {
+			$total_count = $results['total'];
+			unset( $results['total'] );
+		}
+
+		if ( ! empty( $results ) ) {
+			ksort( $results );
+			$keys = array_keys( $results );
+
+			$min_year = $keys[0];
+			$max_year = $keys[ count( $keys ) - 1 ];
+
+			$years = array_map( 'strval', range( (int) $min_year, (int) $max_year, 1 ) );
+		} else {
+			$years = array( (string) current_time( 'Y' ) );
+		}
+
+		$tabs = array(
+			'total' => array(
+				'label'    => _x( 'Total', 'submission count', 'torro-forms' ),
+				'callback' => function() use ( $results, $years, $total_count ) {
+					$year_results = array();
+					foreach ( $years as $year ) {
+						if ( isset( $results[ $year ]['total'] ) ) {
+							$year_results[] = (int) $results[ $year ]['total'];
+						} else {
+							$year_results[] = 0;
+						}
+					}
+
+					?>
+					<p>
+						<strong>
+							<?php _e( 'Number of submissions:', 'torro-forms' ); ?>
+							<?php echo absint( $total_count ); ?>
+						</strong>
+					</p>
+					<div id="<?php echo esc_attr( $this->slug . '-chart-total' ); ?>"></div>
+					<script type="application/json" class="c3-chart-data">
+						<?php echo json_encode( $this->get_chart_json( esc_attr( $this->slug . '-chart-total' ), $years, $year_results, __( 'Years', 'torro-forms' ), __( 'Submission Count', 'torro-forms' ) ) ); ?>
+					</script>
+					<?php
+				},
+			),
+		);
+
+		foreach ( $years as $year ) {
+			$tabs[ $year ] = array(
+				'label'    => $year,
+				'callback' => function() use ( $results, $year ) {
+					global $wp_locale;
+
+					$total_count = isset( $results[ $year ]['total'] ) ? $results[ $year ]['total'] : 0;
+
+					$months = $wp_locale->month;
+
+					$month_results = array();
+					foreach ( $months as $index => $month ) {
+						if ( isset( $results[ $year ][ $index ] ) ) {
+							$month_results[] = (int) $results[ $year ][ $index ];
+						} else {
+							$month_results[] = 0;
+						}
+					}
+
+					?>
+					<p>
+						<strong>
+							<?php _e( 'Number of submissions:', 'torro-forms' ); ?>
+							<?php echo absint( $total_count ); ?>
+						</strong>
+					</p>
+					<div id="<?php echo esc_attr( $this->slug . '-chart-' . $year ); ?>"></div>
+					<script type="application/json" class="c3-chart-data">
+						<?php echo json_encode( $this->get_chart_json( esc_attr( $this->slug . '-chart-' . $year ), array_values( $months ), $month_results, __( 'Months', 'torro-forms' ), __( 'Submission Count', 'torro-forms' ) ) ); ?>
+					</script>
+					<?php
+				},
+			);
+		}
+
+		$this->display_tabs( $tabs );
 	}
 
 	/**
@@ -75,7 +181,7 @@ class General_Stats extends Evaluator implements Assets_Submodule_Interface {
 	 * @param Assets $assets The plugin assets instance.
 	 */
 	public function register_assets( $assets ) {
-
+		// Empty method body.
 	}
 
 	/**
@@ -101,7 +207,8 @@ class General_Stats extends Evaluator implements Assets_Submodule_Interface {
 	 * @param array  $args   Arguments to tweak the displayed results.
 	 */
 	public function enqueue_submission_results_assets( $assets, $form, $args = array() ) {
-
+		$assets->enqueue_script( 'c3' );
+		$assets->enqueue_style( 'c3' );
 	}
 
 	/**
@@ -135,5 +242,56 @@ class General_Stats extends Evaluator implements Assets_Submodule_Interface {
 		);
 
 		return $meta_fields;
+	}
+
+	protected function get_chart_json( $id, $x_values, $y_values, $x_label, $y_label ) {
+		$less_than_10 = true;
+		foreach ( $y_values as $y_value ) {
+			if ( $y_value > 10 ) {
+				$less_than_10 = false;
+				break;
+			}
+		}
+
+		array_unshift( $x_values, 'x' );
+		array_unshift( $y_values, 'submissionCount' );
+
+		$data = array(
+			'bindto' => '#' . $id,
+			'data'   => array(
+				'x'       => 'x',
+				'columns' => array( $x_values, $y_values ),
+				'names'   => array(
+					'submissionCount' => $y_label,
+				),
+				'type'    => 'line',
+				'colors'  => array(
+					'submissionCount' => '#0073aa',
+				),
+			),
+			'axis'   => array(
+				'x' => array(
+					'label' => $x_label,
+					'type' => 'category',
+				),
+				'y' => array(
+					'label' => $y_label,
+					'tick'  => array(
+						'time' => array(
+							'interval' => 1,
+						),
+					),
+					'min'   => 1,
+				),
+			),
+			'legend' => array(
+				'show' => false,
+			),
+		);
+		if ( $less_than_10 ) {
+			$data['axis']['y']['max'] = 10;
+		}
+
+		return $data;
 	}
 }
