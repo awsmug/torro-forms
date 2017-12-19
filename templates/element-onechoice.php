6fb779d0b86568554f3db0c15e9d80edc894e748
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-12-19 16:18:29 +0100

    Fixed frontend styling (fixes #368).

--- templates/element-onechoice.php
+++ templates/element-onechoice.php
@@ -29,10 +29,12 @@
 			$choice_label_attrs['id']  = str_replace( '%index%', $index + 1, $choice_label_attrs['id'] );
 			$choice_label_attrs['for'] = str_replace( '%index%', $index + 1, $choice_label_attrs['for'] );
 			?>
-			<input type="radio"<?php echo torro()->template()->attrs( $choice_input_attrs ); ?> value="<?php echo torro()->template()->esc_attr( $choice ); ?>"<?php echo $value === $choice ? ' checked' : ''; ?>>
-			<label<?php echo torro()->template()->attrs( $choice_label_attrs ); ?>>
-				<?php echo torro()->template()->esc_html( $choice ); ?>
-			</label>
+			<div class="torro-toggle">
+				<input type="radio"<?php echo torro()->template()->attrs( $choice_input_attrs ); ?> value="<?php echo torro()->template()->esc_attr( $choice ); ?>"<?php echo $value === $choice ? ' checked' : ''; ?>>
+				<label<?php echo torro()->template()->attrs( $choice_label_attrs ); ?>>
+					<?php echo torro()->template()->esc_html( $choice ); ?>
+				</label>
+			</div>
 		<?php endforeach; ?>
 
 		<?php if ( ! empty( $description ) ) : ?>
