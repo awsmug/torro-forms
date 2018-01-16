ecf731104463f9532cb9bc3486fa0f54a85ed9dd
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-01-16 16:39:28 +0100

    Add description for required indicator, only displayed visually.

--- templates/container.php
+++ templates/container.php
@@ -2,7 +2,7 @@
 /**
  * Template: container.php
  *
- * Available data: $id, $form_id, $label, $sort, $elements
+ * Available data: $id, $form_id, $label, $sort, $required_description, $elements
  *
  * @package TorroForms
  * @since 1.0.0
@@ -11,6 +11,10 @@
 ?>
 <?php if ( ! empty( $label ) ) : ?>
 	<h2 class="torro-container-title"><?php echo torro()->template()->esc_kses_basic( $label ); ?></h2>
+<?php endif; ?>
+
+<?php if ( ! empty( $required_description ) ) : ?>
+	<p><?php echo torro()->template()->esc_kses_basic( $required_description ); ?></p>
 <?php endif; ?>
 
 <?php foreach ( $elements as $element ) : ?>
