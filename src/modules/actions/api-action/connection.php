1cd6a185b92514ed6a9579c48d98d8e6b5dbfda9
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-01-31 17:46:23 +0100

    Readd API_Action code.

---
+++ src/modules/actions/api-action/connection.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * API connection base class
+ *
+ * @package TorroForms
+ * @since 1.0.0
+ */
+
+namespace awsmug\Torro_Forms\Modules\Actions\API_Action;
+
+use awsmug\Torro_Forms\Modules\Actions\API_Action;
+
+/**
+ * Base class for an API connection.
+ *
+ * @since 1.0.0
+ */
+abstract class Connection {
+
+	/**
+	 * The connection slug.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	protected $slug = '';
+
+	/**
+	 * The connection title.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	protected $title = '';
+
+	/**
+	 * The parent API action.
+	 *
+	 * @since 1.0.0
+	 * @var API_Action|null
+	 */
+	protected $api_action = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * Sets the connection data.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param API_Action $api_action Parent API action.
+	 * @param array      $data       Connection data array.
+	 */
+	public function __construct( $api_action, $data ) {
+		$this->api_action = $api_action;
+
+		foreach ( $data as $key => $value ) {
+			if ( property_exists( $this, $key ) ) {
+				$this->$key = $value;
+			}
+		}
+
+		if ( empty( $this->slug ) ) {
+			$this->slug = sanitize_title( $this->title );
+		}
+	}
+
+	/**
+	 * Gets information about this connection.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $field Field slug to get the value for.
+	 * @return string Value for the field, or empty string if invalid field.
+	 */
+	public function get( $field ) {
+		if ( 'api_action' === $field || ! isset( $this->$field ) ) {
+			return '';
+		}
+
+		return $this->$field;
+	}
+
+	/**
+	 * Gets the array of authentication data.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array Authentication data as $key => $value pairs.
+	 */
+	public function get_authentication_data() {
+		$data = get_object_vars( $this );
+
+		unset( $data['slug'], $data['title'], $data['api_action'] );
+
+		return $data;
+	}
+}
---
+++ src/modules/actions/api-action/x-account-connection.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * X-Account API connection class
+ *
+ * @package TorroForms
+ * @since 1.0.0
+ */
+
+namespace awsmug\Torro_Forms\Modules\Actions\API_Action;
+
+/**
+ * Class for an API connection via X-Account header authentication.
+ *
+ * @since 1.0.0
+ */
+class X_Account_Connection extends Connection {
+
+	/**
+	 * The account placeholder name.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	protected $placeholder_name = 'account';
+
+	/**
+	 * The account identifier.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	protected $account = '';
+
+	/**
+	 * The authorization header name.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	protected $header_name = 'Authorization';
+
+	/**
+	 * The authorization token.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	protected $token = '';
+}
