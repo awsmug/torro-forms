89c9be93b9fc2820d53d73325031c59707b22102
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-09-20 15:13:59 +0200

    Add element context to its CSS classes filters (fixes #464).

--- src/db-objects/elements/element.php
+++ src/db-objects/elements/element.php
@@ -196,46 +196,56 @@ class Element extends Model {
 		 * Filters the element input classes.
 		 *
 		 * @since 1.0.0
+		 * @since 1.1.0 Added the $element parameter.
 		 *
-		 * @param array $input_classes Array of input classes.
+		 * @param array   $input_classes Array of input classes.
+		 * @param Element $element       Element object.
 		 */
-		$input_classes = apply_filters( "{$this->manager->get_prefix()}element_input_classes", array( 'torro-element-input' ) );
+		$input_classes = apply_filters( "{$this->manager->get_prefix()}element_input_classes", array( 'torro-element-input' ), $this );
 
 		/**
 		 * Filters the element label classes.
 		 *
 		 * @since 1.0.0
+		 * @since 1.1.0 Added the $element parameter.
 		 *
-		 * @param array $label_classes Array of label classes.
+		 * @param array   $label_classes Array of label classes.
+		 * @param Element $element       Element object.
 		 */
-		$label_classes = apply_filters( "{$this->manager->get_prefix()}element_label_classes", array( 'torro-element-label' ) );
+		$label_classes = apply_filters( "{$this->manager->get_prefix()}element_label_classes", array( 'torro-element-label' ), $this );
 
 		/**
 		 * Filters the element wrap classes.
 		 *
 		 * @since 1.0.0
+		 * @since 1.1.0 Added the $element parameter.
 		 *
-		 * @param array $wrap_classes Array of wrap classes.
+		 * @param array   $wrap_classes Array of wrap classes.
+		 * @param Element $element      Element object.
 		 */
-		$wrap_classes = apply_filters( "{$this->manager->get_prefix()}element_wrap_classes", array( 'torro-element-wrap' ) );
+		$wrap_classes = apply_filters( "{$this->manager->get_prefix()}element_wrap_classes", array( 'torro-element-wrap' ), $this );
 
 		/**
 		 * Filters the element description classes.
 		 *
 		 * @since 1.0.0
+		 * @since 1.1.0 Added the $element parameter.
 		 *
-		 * @param array $description_classes Array of description classes.
+		 * @param array   $description_classes Array of description classes.
+		 * @param Element $element             Element object.
 		 */
-		$description_classes = apply_filters( "{$this->manager->get_prefix()}element_description_classes", array( 'torro-element-description' ) );
+		$description_classes = apply_filters( "{$this->manager->get_prefix()}element_description_classes", array( 'torro-element-description' ), $this );
 
 		/**
 		 * Filters the element errors classes, for the error messages wrap.
 		 *
 		 * @since 1.0.0
+		 * @since 1.1.0 Added the $element parameter.
 		 *
-		 * @param array $errors_classes Array of errors classes.
+		 * @param array   $errors_classes Array of errors classes.
+		 * @param Element $element        Element object.
 		 */
-		$errors_classes = apply_filters( "{$this->manager->get_prefix()}element_errors_classes", array( 'torro-element-errors' ) );
+		$errors_classes = apply_filters( "{$this->manager->get_prefix()}element_errors_classes", array( 'torro-element-errors' ), $this );
 
 		$data = array_merge(
 			$data,
