a832b5f0a47d6a26bd5fb852f9a4a1c5bc048e72
Sven Wagener <sven@awesome.ug>
2019-01-17 09:40:13 +0100

    Removed test class

--- src/torro-forms.php
+++ src/torro-forms.php
@@ -95,19 +95,3 @@ function torro_load( $callback ) {
 }
 
 torro();
-
-class NewsletterApi extends awsmug\Torro_Forms\Modules\Actions\Action {
-	protected function bootstrap() {
-		$this->slug  = 'newsletter-api';
-		$this->title = 'Newsletter API';
-	}
-
-	public function handle( $submission, $form ) {
-		wp_die($submission);
-	}
-}
-
-torro_load( function() {
-	var_dump( $_SERVER['REMOTE_ADDR'] );
-	torro()->modules()->actions()->register( 'newsletterapi', 'NewsletterApi' );
-});
