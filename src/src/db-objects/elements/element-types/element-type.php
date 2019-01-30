1b6bbcc94d05c3eb4c97d6fbe827694a51b97e4f
Sven Wagener <sven@awesome.ug>
2019-01-30 11:55:12 +0100

    Added filter torro_export_value

--- src/src/db-objects/elements/element-types/element-type.php
+++ src/src/db-objects/elements/element-types/element-type.php
@@ -349,6 +349,16 @@ abstract class Element_Type {
 			$value = implode( ', ', $value );
 		}
 
+		/**
+		 * Filters the value for export
+		 *
+		 * @since 1.0.5
+		 *
+		 * @param string  $value    Value to filter.
+		 * @param Element $element  Element object.
+		 */
+		$value = apply_filters( "{$this->manager->get_prefix()}export_value", $value, $element );
+
 		return array(
 			'element_' . $element->id . '__main' => $this->escape_single_value_for_export( $value, $export_format ),
 		);
