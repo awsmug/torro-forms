f004c1a1f3b26f01088802d9ec7f941844316b7e
Sven Wagener <sven@awesome.ug>
2019-04-10 15:06:17 +0200

    Got range working for react

--- src/assets/src/js/Form/Elements/Range.js
+++ src/assets/src/js/Form/Elements/Range.js
@@ -3,13 +3,45 @@ import Element from "./Element";
 /**
  * Textarea element.
  *
- * @since 1.1.0
+ * @since 1.2.0
  */
 class Range extends Element {
+
+	/**
+	 * Syncing Change in range to helper input.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @param event
+	 */
+	rangeChange( event ) {
+		const input = event.target;
+		const value = input.value;
+
+		const helper_input = input.closest( '.torro-element-range' ).querySelector('.torro-helper-input input' );
+		helper_input.value = value;
+	}
+
+	/**
+	 * Syncing Change in helper input to range.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @param event
+	 */
+	rangeChangeBack( event ) {
+		const helper_input = event.target;
+		const value = helper_input.value;
+
+		const input = helper_input.closest( '.torro-element-range' ).querySelector('.torro-input input' );
+
+		input.value = value;
+	}
+
 	/**
 	 * Rendering element.
 	 *
-	 * @since 1.1.0
+	 * @since 1.2.0
 	 */
 	renderElement( params ) {
 		const element = this.state.element;
@@ -22,7 +54,7 @@ class Range extends Element {
 		if( element.helper_input === 'before' ) {
 			helper_before = (
 				<div className="torro-helper-input">
-					<input type="text" className={element.helper_input_attrs.class} size={element.helper_input_attrs.size} maxlength={element.helper_input_attrs.maxlength} />
+					<input type="text" className={element.helper_input_attrs.class} size={element.helper_input_attrs.size} maxLength={element.helper_input_attrs.maxlength} onChange={event => this.rangeChangeBack(event)} />
 				</div>
 			);
 		};
@@ -47,7 +79,7 @@ class Range extends Element {
 				<div>
 					{helper_before}
 						<div className="torro-input">
-							<input type="range" min={element.input_attrs.min} max={element.input_attrs.max} step={element.input_attrs.step} onChange={} />
+							<input type="range" min={element.input_attrs.min} max={element.input_attrs.max} step={element.input_attrs.step} onChange={event => this.rangeChange(event)} />
 						</div>
 					{helper_after}
 				</div>
