35e5cf9dfc2b7d640ea7e7a92206722a06b51062
Sven Wagener <sven@awesome.ug>
2019-07-10 14:44:28 +0200

    Not using filter_has_varm, because it's part of the filter_input PHP bug

--- src/vendor/felixarntz/plugin-lib/src/fixes.php
+++ src/vendor/felixarntz/plugin-lib/src/fixes.php
@@ -20,17 +20,17 @@ if ( ! trait_exists( 'Leaves_And_Love\Plugin_Lib\Traits\Fixes_Trait' ) ) :
 	class Fixes {
 		/**
 		 * PHP filter_input function fix.
-		 * 
+		 *
 		 * Wrapper for filter_input INPUT_SERVER/INPUT_ENV bug in PHP in fast-cgi configured php installations.
 		 * @see https://php.net/manual/de/function.filter-input.php#77307
-		 * 
+		 *
 		 * @since 1.0.5
-		 * 
+		 *
 		 * @param int    $type          One of INPUT_SERVER or INPUT_ENV.
 		 * @param string $variable_name Name of a variable to get.
 		 * @param int    $filter        The ID of the filter to apply.
-		 * @param mixed  $options       Associative array of options or bitwise disjunction of flags. 
-		 * 
+		 * @param mixed  $options       Associative array of options or bitwise disjunction of flags.
+		 *
 		 * @return mixed $value         Value of the requested variable on success, FALSE if the filter fails, or NULL if the variable_name variable is not set.
 		 */
 		public static function php_filter_input( $type, $variable_name, $filter = FILTER_DEFAULT, $options = null  ) {
@@ -40,18 +40,15 @@ if ( ! trait_exists( 'Leaves_And_Love\Plugin_Lib\Traits\Fixes_Trait' ) ) :
 				return $value;
 			}
 
-			if ( ! filter_has_var( $type, $variable_name ) ) {
-				return $value;
+			if ( INPUT_SERVER === $type && array_key_exists( $variable_name, $_SERVER ) ) {
+				return filter_var( wp_unslash( $_SERVER[ $variable_name ] ), $filter, $options );
 			}
 
-			switch( $type ) {
-				case INPUT_SERVER:
-					return filter_var( wp_unslash( $_SERVER[ $variable_name ] ), $filter, $options );
-				case INPUT_ENV:
-					return filter_var( $_ENV[ $variable_name ], $filter, $options );
-				default:
-					return $value;
+			if ( INPUT_ENV === $type && array_key_exists( $variable_name, $_SERVER ) ) {
+				return filter_var( wp_unslash( $_ENV[ $variable_name ] ), $filter, $options );
 			}
+
+			return $value;
 		}
 	}
 
