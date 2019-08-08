1441488b518de96a6c7be0a945fbdf2422275f95
Sven Wagener <sven@awesome.ug>
2019-08-08 13:39:22 +0200

    Added settings fields and rendering output.

--- src/src/modules/frontends/module.php
+++ src/src/modules/frontends/module.php
@@ -3,24 +3,24 @@
  * Frontends module class
  *
  * @package TorroForms
- * @since 1.2.0
+ * @since 1.1.0
  */
 
 namespace awsmug\Torro_Forms\Modules\Frontends;
-
-use awsmug\Torro_Forms\DB_Objects\Forms\Form_Frontend_Output_Handler;
-use awsmug\Torro_Forms\DB_Objects\Forms\Form;
-use awsmug\Torro_Forms\DB_Objects\Submissions\Submission;
 
 use awsmug\Torro_Forms\Modules\Hooks_Submodule_Interface;
 use awsmug\Torro_Forms\Modules\Module as Module_Base;
 use awsmug\Torro_Forms\Modules\Submodule_Registry_Interface;
 use awsmug\Torro_Forms\Modules\Submodule_Registry_Trait;
 
+use awsmug\Torro_Forms\DB_Objects\Forms\Form_Frontend_Output_Handler;
+use awsmug\Torro_Forms\DB_Objects\Forms\Form;
+use awsmug\Torro_Forms\DB_Objects\Submissions\Submission;
+
 /**
  * Class for the Frontends module.
  *
- * @since 1.2.0
+ * @since 1.1.0
  */
 class Module extends Module_Base implements Submodule_Registry_Interface {
 	use Submodule_Registry_Trait;
@@ -33,7 +33,8 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	protected function bootstrap() {
 		$this->slug        = 'frontends';
 		$this->title       = __( 'Frontends', 'torro-forms' );
-		$this->description = __( 'Frondends allowing to use different types of Frontends', 'torro-forms' );
+		$this->description = __( 'Frontends allowing to use different types of Frontends', 'torro-forms' );
+		$this->required    = true;
 
 		$this->submodule_base_class = Frontend::class;
 		$this->default_submodules   = array(
@@ -51,7 +52,12 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	 * @param Submission                   $submission     Current submission.
 	 */
 	public function render_output( $output_handler, $form, $submission ) {
+		$options = $this->manager()->options()->get( 'general_settings', array() );
+
 		$frontend_slug = 'standard';
+		if ( array_key_exists( 'frontend_slug', $options ) ) {
+			$frontend_slug = $options['frontend_slug'];
+		}
 
 		$frontend = $this->submodules[ $frontend_slug ];
 
@@ -100,12 +106,50 @@ class Module extends Module_Base implements Submodule_Registry_Interface {
 	}
 
 	/**
+	 * Choosing frontend from general settings page.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param array $tabs Associative array of `$field_slug => $field_args` pairs.
+	 *
+	 * @return array $tabs Filtered associative array of `$field_slug => $field_args` pairs.
+	 */
+	public function general_settings_fields( $fields ) {
+		if ( 1 >= count( $this->submodules ) ) {
+			// return $fields;
+		}
+
+		$choices = array();
+		foreach ( $this->submodules as $submodule ) {
+			$choices [ $submodule->get_slug() ] = $submodule->get_title();
+		}
+
+		$fields['frontend_slug'] = array(
+			'section'     => 'form_behavior',
+			'type'        => 'select',
+			'label'       => __( 'Frontend', 'torro-forms' ),
+			'description' => __( 'The frontend engine which will be used to display forms.', 'torro-forms' ),
+			'default'     => 'standard',
+			'choices'     => $choices,
+		);
+
+		return $fields;
+	}
+
+	/**
 	 * Sets up all action and filter hooks for the service.
 	 *
 	 * @since 1.1.0
 	 */
 	protected function setup_hooks() {
 		parent::setup_hooks();
+
+		$this->filters[] = array(
+			'name'     => $this->get_prefix() . 'settings_fields',
+			'callback' => array( $this, 'general_settings_fields' ),
+			'priority' => 100,
+			'num_args' => 1,
+		);
 		$this->actions[] = array(
 			'name'     => $this->get_prefix() . 'frontend_output_handler',
 			'callback' => array( $this, 'render_output' ),
