a3eb6d08be987a64350fdde061beeba991d24163
Sven Wagener <sven@awesome.ug>
2019-08-08 13:38:35 +0200

    We cann not continue here yet. We have to hide reqired modules in annother way.

--- src/src/db-objects/forms/form-settings-page.php
+++ src/src/db-objects/forms/form-settings-page.php
@@ -780,7 +780,7 @@ class Form_Settings_Page extends Tabbed_Settings_Page {
 		$modules = array();
 		foreach ( torro()->modules()->get_all() as $slug => $module ) {
 			if( $module->is_required() ) {
-				continue;
+				// continue;
 			}
 			$modules[ $slug ] = $module->get_title();
 		}
